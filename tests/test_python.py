Granulate/gprofiler

commit 3e9381bc4ff79ffdbd9b3cdf3eab25cf63cfc91f

Select processes to profile based on "python"/"java" in their exe's basename (#5)
This is more accurate than "pgrep python", because a Python script exec'd directly won't
have "python" in its comm (hence the different PYTHON_KNOWN_PROCS we pgrep'd for).
This new approach is more accurate, and it can be taken further to actually finding
processes with ".*python.*" in their /proc/pid/maps file.

Also did the same change for Java, along the way.

gprofiler/python.py
@@ -9,7 +9,7 @@ from threading import Event
 from typing import Dict
 
 from .exceptions import StopEventSetException, ProcessStoppedException
-from .utils import pgrep, run_process, resource_path
+from .utils import pgrep_exe, run_process, resource_path
 
 logger = logging.getLogger(__name__)
 
@@ -18,7 +18,6 @@ PYTHON_PROFILER_MAX_FREQUENCY = 10
 
 class PythonProfiler:
     BLACKLISTED_PYTHON_PROCS = ["unattended-upgrades", "networkd-dispatcher", "supervisord", "tuned"]
-    PYTHON_KNOWN_PROCS = ["python", "uwsgi", "gunicorn", "celery", "ipython", "python3"]
 
     def __init__(self, frequency: int, duration: int, stop_event: Event, storage_dir: str):
         self._frequency = min(frequency, PYTHON_PROFILER_MAX_FREQUENCY)
@@ -69,7 +68,7 @@ class PythonProfiler:
 
     def find_python_processes_to_profile(self) -> Dict[str, str]:
         filtered_procs = {}
-        for process in pgrep("^{}$".format("|".join(self.PYTHON_KNOWN_PROCS))):
+        for process in pgrep_exe(r"^.+/python[^/]*$"):
             try:
                 if process.pid == os.getpid():
                     continue

gprofiler/utils.py
@@ -10,7 +10,7 @@ import subprocess
 from functools import lru_cache
 from subprocess import CompletedProcess, Popen, TimeoutExpired
 from threading import Event
-from typing import Union, List, Optional
+from typing import Iterator, Union, List, Optional
 
 import importlib_resources
 import psutil
@@ -100,9 +100,9 @@ def run_process(
     return result
 
 
-def pgrep(match: str) -> List[Process]:
+def pgrep_exe(match: str) -> Iterator[Process]:
     pattern = re.compile(match)
-    return [process for process in psutil.process_iter() if pattern.match(process.name())]
+    return (process for process in psutil.process_iter() if pattern.match(process.exe()))
 
 
 def get_iso8061_format_time(time: datetime.datetime) -> str:

gprofiler/java.py
@@ -15,7 +15,7 @@ import psutil
 from psutil import Process
 
 from .exceptions import StopEventSetException
-from .utils import run_process, pgrep, get_self_container_id, resource_path
+from .utils import run_process, pgrep_exe, get_self_container_id, resource_path
 
 logger = logging.getLogger(__name__)
 
@@ -149,7 +149,7 @@ class JavaProfiler:
     def profile_processes(self):
         futures = []
         results = {}
-        processes = pgrep("^java$")
+        processes = list(pgrep_exe(r"^.+/java$"))
         if not processes:
             return {}
         with concurrent.futures.ThreadPoolExecutor(max_workers=len(processes)) as executor:

tests/conftest.py
@@ -42,6 +42,8 @@ def java_command_line(class_path: Path) -> List:
 def command_line(tmp_path: Path, runtime: str) -> List:
     return {
         "java": java_command_line(tmp_path / "java"),
+        # note: here we run "python /path/to/fibonacci.py" while in the container test we have
+        # "CMD /path/to/fibonacci.py", to test processes with non-python /proc/pid/comm
         "python": ["python3", CONTAINERS_DIRECTORY / "python/fibonacci.py"],
     }[runtime]
 

tests/containers/python/Dockerfile
@@ -3,4 +3,4 @@ FROM python:3.6-alpine
 WORKDIR /app
 ADD fibonacci.py /app
 
-CMD ["python", "/app/fibonacci.py"]
+CMD ["/app/fibonacci.py"]

tests/containers/python/fibonacci.py
@@ -1,3 +1,4 @@
+#!/usr/local/bin/python
 #
 # Copyright (c) Granulate. All rights reserved.
 # Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.

commit 940c3aeea803b798d4f9513573a9c63f9b4a6b57

python: Select processes to profile based on python/libpython in their maps
In 3e9381bc4ff7 I suggested this change as an improvement, but it's actually required
because we currently miss executables which load libpython, such as uwsgi.

This change means that we'll prefer the Python samples from *any* process which has
libpython loaded, even if it doesn't even run Python code now. We might need to address
that later, by making smarter decisions on the source for our samples.

Fixes: 3e9381bc4ff7

gprofiler/python.py
@@ -11,7 +11,7 @@ from typing import Dict
 
 from .merge import parse_collapsed
 from .exceptions import StopEventSetException, ProcessStoppedException
-from .utils import pgrep_exe, run_process, resource_path
+from .utils import pgrep_maps, run_process, resource_path
 
 logger = logging.getLogger(__name__)
 
@@ -70,7 +70,7 @@ class PythonProfiler:
 
     def find_python_processes_to_profile(self) -> Dict[str, str]:
         filtered_procs = {}
-        for process in pgrep_exe(r"^.+/python[^/]*$"):
+        for process in pgrep_maps(r"^.+/(?:lib)?python[^/]*$"):
             try:
                 if process.pid == os.getpid():
                     continue

gprofiler/utils.py
@@ -105,5 +105,10 @@ def pgrep_exe(match: str) -> Iterator[Process]:
     return (process for process in psutil.process_iter() if pattern.match(process.exe()))
 
 
+def pgrep_maps(match: str) -> Iterator[Process]:
+    pattern = re.compile(match)
+    return (process for process in psutil.process_iter() if any(pattern.match(m.path) for m in process.memory_maps()))
+
+
 def get_iso8061_format_time(time: datetime.datetime) -> str:
     return time.replace(microsecond=0).isoformat()

tests/containers/python/Dockerfile.libpython
@@ -1,0 +1,10 @@
+FROM python:3.6-alpine
+
+WORKDIR /app
+ADD fibonacci.py /app
+# this is used to test that we identify Python processes to profile based on "libpython" in their "/proc/pid/maps".
+# so we'll run a Python script using non-"python" executable ("shmython" instead) but it'll have "libpython"
+# loaded.
+RUN ln /usr/local/bin/python3.6 /usr/local/bin/shmython && ! test -L /usr/local/bin/shmython && ldd /usr/local/bin/shmython | grep libpython > /dev/null
+
+CMD ["/usr/local/bin/shmython", "/app/fibonacci.py"]

tests/test_python.py
@@ -1,0 +1,38 @@
+#
+# Copyright (c) Granulate. All rights reserved.
+# Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
+#
+from docker import DockerClient
+from docker.models.images import Image
+import pytest
+
+from gprofiler.python import PythonProfiler
+
+from tests.conftest import CONTAINERS_DIRECTORY  # type: ignore
+
+
+@pytest.fixture(scope="session")
+def application_docker_image(docker_client: DockerClient, runtime: str) -> Image:
+    dockerfile = CONTAINERS_DIRECTORY / runtime / "Dockerfile.libpython"
+    image: Image = docker_client.images.build(path=str(dockerfile.parent), dockerfile=str(dockerfile))[0]
+    yield image
+    docker_client.images.remove(image.id, force=True)
+
+
+@pytest.fixture(scope="session")
+def runtime(request) -> str:
+    return "python"
+
+
+def test_python_select_by_libpython(
+    profiler: PythonProfiler,
+    application_docker_container,
+    output_directory,
+    assert_collapsed,
+) -> None:
+    """
+    Tests that profiling of processes running Python, whose basename(readlink("/proc/pid/exe")) isn't "python".
+    (for example, uwsgi). We expect to select these because they have "libpython" in their "/proc/pid/maps".
+    """
+    process_collapsed = profiler.profile_processes()
+    assert_collapsed(process_collapsed.get(application_docker_container.attrs["State"]["Pid"]))

tests/test_sanity.py
@@ -17,7 +17,7 @@ from gprofiler.merge import parse_collapsed
 def test_from_host(
     application_pid: int,
     profiler: Union[JavaProfiler, PythonProfiler],
-    assert_collapsed: Callable[[Mapping[str, int]], None]
+    assert_collapsed: Callable[[Mapping[str, int]], None],
 ) -> None:
     process_collapsed = profiler.profile_processes()
     assert_collapsed(process_collapsed.get(application_pid))