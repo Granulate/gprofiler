b2efb89c5d038f5f82e9b7caa0fda490781de3ef
David Dorfman <david.dorfman@granulate.io>
2021-04-09 20:35:59 +0300

    tests: More fine-grained testing for specific runtimes or profilers. (#19)
    The `profiler` fixture assumes only a single profiler implementation for a
    particular runtime. This change inlines it and requires unit tests to be
    written for a particular implementation. The existing test for the gprofiler
    docker image remains the same. In addition, we now no longer execute test
    applications of runtimes that are not being tested, and we don't execute an
    application both within a container and on the host for the same test
    permutation.
    
    After this change, it is easier to write tests for a particular profiler
    implementation, or for a particular application runtime.

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) Granulate. All rights reserved.
+# Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
+#
+from pathlib import Path
+
+HERE = Path(__file__).parent
+PARENT = HERE.parent
+CONTAINERS_DIRECTORY = HERE / "containers"
--- tests/test_python.py
+++ tests/test_python.py
@@ -2,30 +2,32 @@
 # Copyright (c) Granulate. All rights reserved.
 # Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
 #
+import pytest  # type: ignore
 from docker import DockerClient
 from docker.models.images import Image
-import pytest  # type: ignore
+from threading import Event
 
 from gprofiler.python import PythonProfiler
 
-from tests.conftest import CONTAINERS_DIRECTORY  # type: ignore
+from tests import CONTAINERS_DIRECTORY  # type: ignore
+
+
+@pytest.fixture
+def runtime() -> str:
+    return "python"
 
 
 @pytest.fixture(scope="session")
-def application_docker_image(docker_client: DockerClient, runtime: str) -> Image:
-    dockerfile = CONTAINERS_DIRECTORY / runtime / "Dockerfile.libpython"
+def application_docker_image(docker_client: DockerClient) -> Image:
+    dockerfile = CONTAINERS_DIRECTORY / 'python' / "Dockerfile.libpython"
     image: Image = docker_client.images.build(path=str(dockerfile.parent), dockerfile=str(dockerfile))[0]
     yield image
     docker_client.images.remove(image.id, force=True)
 
 
-@pytest.fixture(scope="session")
-def runtime(request) -> str:
-    return "python"
-
-
+@pytest.mark.parametrize('in_container', [True])
 def test_python_select_by_libpython(
-    profiler: PythonProfiler,
+    tmp_path,
     application_docker_container,
     output_directory,
     assert_collapsed,
@@ -34,5 +36,6 @@ def test_python_select_by_libpython(
     Tests that profiling of processes running Python, whose basename(readlink("/proc/pid/exe")) isn't "python".
     (for example, uwsgi). We expect to select these because they have "libpython" in their "/proc/pid/maps".
     """
+    profiler = PythonProfiler(1000, 1, Event(), str(tmp_path))
     process_collapsed = profiler.profile_processes()
     assert_collapsed(process_collapsed.get(application_docker_container.attrs["State"]["Pid"]))
