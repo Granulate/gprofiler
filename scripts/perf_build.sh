41e52708a1b605edba30e3b68c510d3fa58c3d17
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2021-05-10 18:43:15 +0300

    perf: Upgrade to 40a7823cf90a7e69ce8af88d224dfdd7e371de2d (v5.12g2)

--- scripts/perf_build.sh
+++ scripts/perf_build.sh
@@ -5,7 +5,7 @@
 #
 set -euo pipefail
 
-curl -sSL https://codeload.github.com/Granulate/linux/zip/5ad1bebfc3ed2d1255f11986c627d77a15912710 -o linux.zip
+curl -sSL https://codeload.github.com/Granulate/linux/zip/40a7823cf90a7e69ce8af88d224dfdd7e371de2d -o linux.zip
 unzip linux.zip
 cd linux
 make -C tools/perf LDFLAGS=-static
