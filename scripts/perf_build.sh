6a83b486a9113e9a8617b62d5746b019d8d6e66a
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2021-05-11 03:38:11 +0300

    scripts: curl w/o -s
    No need for silent

--- scripts/perf_build.sh
+++ scripts/perf_build.sh
@@ -5,7 +5,7 @@
 #
 set -euo pipefail
 
-curl -sSL https://codeload.github.com/Granulate/linux/zip/40a7823cf90a7e69ce8af88d224dfdd7e371de2d -o linux.zip
+curl -SL https://codeload.github.com/Granulate/linux/zip/40a7823cf90a7e69ce8af88d224dfdd7e371de2d -o linux.zip
 unzip linux.zip
 cd linux
 make -C tools/perf LDFLAGS=-static
