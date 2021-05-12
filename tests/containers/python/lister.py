ab54f67386b836d17a3bba7b612b556f4cea1b7e
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2021-05-12 20:24:00 +0300

    tests: python: Change the tested app so we get stacks race-less
    If the app does 2 things and we need to catch them both - it's racy.
    This way it's not. We're just listing and we'll always have 'lister' in the stacks,
    and at least some kernel stacks of listing the directory.

--- tests/containers/python/lister.py
+++ tests/containers/python/lister.py
@@ -6,11 +6,10 @@
 import os
 
 
-def fibonacci(n: int) -> int:
-    return n if n <= 1 else fibonacci(n - 1) + fibonacci(n - 2)
+def lister():
+    os.listdir("/")  # have some kernel stacks
 
 
 if __name__ == "__main__":
     while True:
-        os.listdir("/")  # have some kernel stacks
-        fibonacci(15)
+        lister()
