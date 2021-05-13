7c7b679290405bfb6fa6825fd4915beb14f7f599
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2021-05-13 19:46:50 +0300

    Comment on zip download

--- scripts/perf_build.sh
+++ scripts/perf_build.sh
@@ -5,6 +5,7 @@
 #
 set -euo pipefail
 
+# downloading the zip because the git is very large (this is also very large, but still smaller)
 curl -SL https://codeload.github.com/Granulate/linux/zip/40a7823cf90a7e69ce8af88d224dfdd7e371de2d -o linux.zip
 unzip linux.zip
 cd linux-*/
