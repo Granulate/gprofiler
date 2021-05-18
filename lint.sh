f92acea23bec6205460f4e3f1029be9551dacf74
Alon Ben Tamar <alon.ben.tamar@granulate.io>
2021-05-19 01:04:39 +0300

    Fix linting so it would ignore external packages and use the venv if necessary

--- lint.sh
+++ lint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
+if [ -f venv/bin/activate ]; then
+  source venv/bin/activate
+fi
+
 isort --settings-file .isort.cfg .
 flake8 --config .flake8 .
-black --line-length 120 --skip-string-normalization --check .
+black --line-length 120 --skip-string-normalization .
 mypy .
