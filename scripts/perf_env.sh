b9415c6ff681181a3fca86abda3fb5214b890d2e
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2021-05-18 23:06:09 +0300

    perf: Downgrade libunwind & elfutils to what we previously had
    Let's not mix too many changes in one PR

--- scripts/perf_env.sh
+++ scripts/perf_env.sh
@@ -29,11 +29,11 @@ apt-get update && apt-get install -y \
     bison
 
 # install newer versions of elfutils
-curl -L ftp://sourceware.org/pub/elfutils/0.183/elfutils-0.183.tar.bz2 -o /tmp/elfutils-0.183.tar.bz2
-cd /tmp && tar -xf elfutils-0.183.tar.bz2
-cd /tmp/elfutils-0.183 && ./configure --disable-debuginfod --prefix=/usr && make && make install
+curl -L ftp://sourceware.org/pub/elfutils/0.179/elfutils-0.179.tar.bz2 -o /tmp/elfutils-0.179.tar.bz2
+cd /tmp && tar -xf elfutils-0.179.tar.bz2
+cd /tmp/elfutils-0.179 && ./configure --disable-debuginfod --prefix=/usr && make && make install
 
 # & libunwind
-curl -L http://download.savannah.nongnu.org/releases/libunwind/libunwind-1.5.0.tar.gz -o /tmp/libunwind-1.5.0.tar.gz
-cd /tmp && tar -xf libunwind-1.5.0.tar.gz
-cd /tmp/libunwind-1.5.0 && ./configure --prefix=/usr && make install
+curl -L http://download.savannah.nongnu.org/releases/libunwind/libunwind-1.4.0.tar.gz -o /tmp/libunwind-1.4.0.tar.gz
+cd /tmp && tar -xf libunwind-1.4.0.tar.gz
+cd /tmp/libunwind-1.4.0 && ./configure --prefix=/usr && make install
