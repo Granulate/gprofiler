d531aac1ede3053cb9c22e3a2aa28c669d7c4beb
Alon Ben Tamar <alon.ben.tamar@granulate.io>
2021-05-23 11:56:27 +0300

    Add option to run lint script as CI to avoid formatting files

--- lint.sh
+++ lint.sh
@@ -5,7 +5,12 @@ if [ -f venv/bin/activate ]; then
   source venv/bin/activate
 fi
 
+check_arg=""
+if [[ "$1" = "--ci" ]]; then
+    check_arg="--check"
+fi
+
 isort --settings-file .isort.cfg .
 flake8 --config .flake8 .
-black --line-length 120 --skip-string-normalization .
+black --line-length 120 --skip-string-normalization $check_arg .
 mypy .
