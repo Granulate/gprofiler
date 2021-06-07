32ebe87b89ea913adc5621327d104304f55e21a3
Alon Ben Tamar <alon.ben.tamar@granulate.io>
2021-06-07 14:23:25 +0300

    Improve the global perf method to make it more readable

--- gprofiler/perf.py
+++ gprofiler/perf.py
@@ -7,7 +7,7 @@ import logging
 import os
 from tempfile import NamedTemporaryFile
 from threading import Event
-from typing import Tuple
+from typing import Tuple, Optional
 
 import psutil
 
@@ -73,11 +73,18 @@ class SystemProfiler:
         logger.info("Finished running global perf")
         return perf_result
 
-    def _get_global_perf_result(self):
+    def _get_global_perf_result(self) -> Tuple[ProcessToStackSampleCounters, ProcessIdToCommMapping]:
+        fp_perf: Optional[str] = None
+        dwarf_perf: Optional[str] = None
         if not self._fp_perf:
-            return merge_global_perfs(None, self._run_perf(dwarf=True))
-        if not self._dwarf_perf:
-            return merge_global_perfs(self._run_perf(dwarf=False), None)
+            dwarf_perf = self._run_perf(dwarf=True)
+        elif not self._dwarf_perf:
+            fp_perf = self._run_perf(dwarf=False)
+        else:
+            dwarf_perf, fp_perf = self._run_fp_and_dwarf_concurrent_perfs()
+        return merge_global_perfs(fp_perf, dwarf_perf)
+
+    def _run_fp_and_dwarf_concurrent_perfs(self) -> Tuple[str, str]:
         with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
             # We are running 2 perfs in parallel - one with DWARF and one with FP, and then we merge their results.
             # This improves the results from software that is compiled without frame pointers,
@@ -85,6 +92,4 @@ class SystemProfiler:
             # versions of Go processes.
             fp_future = executor.submit(self._run_perf, False)
             dwarf_future = executor.submit(self._run_perf, True)
-        fp_perf = fp_future.result()
-        dwarf_perf = dwarf_future.result()
-        return merge_global_perfs(fp_perf, dwarf_perf)
+        return dwarf_future.result(), fp_future.result()
