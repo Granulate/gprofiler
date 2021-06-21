e582a0b1ad67f3ad887de9e7dc88f87fb54ef112
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2021-06-21 11:34:07 +0300

    Bump PyPerf version

--- scripts/pyperf_build.sh
+++ scripts/pyperf_build.sh
@@ -5,7 +5,7 @@
 #
 set -e
 
-git clone --depth 1 -b v1.0.7 https://github.com/Granulate/bcc.git && cd bcc && git reset --hard 3ffa8982fdbfce183da81af634dc39f743e65d59
+git clone --depth 1 -b v1.0.8 https://github.com/Granulate/bcc.git && cd bcc && git reset --hard 3d45543714db67172a08d1dffaa84d7c5eb83808
 mkdir build
 cd build
 cmake -DPYTHON_CMD=python3 -DINSTALL_CPP_EXAMPLES=y -DCMAKE_INSTALL_PREFIX=/bcc/root ..
