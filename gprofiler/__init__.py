30230d104667b7316af65da69f51f561920bce10
Alon Ben Tamar <alon.ben.tamar@granulate.io>
2021-06-30 16:38:14 +0300

    Add a version dataclass

--- gprofiler/__init__.py
+++ gprofiler/__init__.py
@@ -1,1 +1,16 @@
+from dataclasses import dataclass
+
 __version__ = "1.1.4"
+
+
+@dataclass
+class Version:
+    def __init__(self, version_str: str):
+        self.major, self.minor, self.patch = (int(part) for part in version_str.split("."))
+
+    major: int
+    minor: int
+    patch: int
+
+
+version = Version(__version__)
