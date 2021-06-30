77fbdf9503170df7da52b3a3c90c54ced1a1584a
Alon Ben Tamar <alon.ben.tamar@granulate.io>
2021-06-30 19:40:57 +0300

    Remove unnecessary version

--- gprofiler/__init__.py
+++ gprofiler/__init__.py
@@ -1,16 +1,1 @@
-from dataclasses import dataclass
-
 __version__ = "1.1.4"
-
-
-@dataclass
-class Version:
-    def __init__(self, version_str: str):
-        self.major, self.minor, self.patch = (int(part) for part in version_str.split("."))
-
-    major: int
-    minor: int
-    patch: int
-
-
-version = Version(__version__)
