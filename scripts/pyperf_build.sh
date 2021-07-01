774bd5b30f72479233c992d66a525c412001fab4
Yishai Zinkin <yishai.zinkin@granulate.io>
2021-07-01 19:41:02 +0000

    Fix for CentOS

--- scripts/pyperf_build.sh
+++ scripts/pyperf_build.sh
@@ -5,7 +5,7 @@
 #
 set -e
 
-git clone --shallow-since="Thu Jul 1 12:34:38 2021 +0000" --single-branch -b feature/show-native-stacks https://github.com/Granulate/bcc.git && cd bcc && git reset --hard cfbebc7fd723943d8cad4a23c76af3332dd2d88c
+git clone --depth 6 -b feature/show-native-stacks https://github.com/Granulate/bcc.git && cd bcc && git reset --hard cfbebc7fd723943d8cad4a23c76af3332dd2d88c
 mkdir build
 cd build
 cmake -DPYTHON_CMD=python3 -DINSTALL_CPP_EXAMPLES=y -DCMAKE_INSTALL_PREFIX=/bcc/root ..
