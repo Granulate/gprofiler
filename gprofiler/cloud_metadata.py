98723e675d0bbefb04a05f0f25fecce63b53c601
Alon Ben Tamar <alon.ben.tamar@granulate.io>
2021-07-05 14:39:36 +0300

    Minor fixes

--- gprofiler/cloud_metadata.py
+++ gprofiler/cloud_metadata.py
@@ -7,7 +7,7 @@ from requests import Response
 
 from gprofiler.exceptions import BadResponseCode
 
-AWS_TIMEOUT = 5
+METADATA_REQUEST_TIMEOUT = 5
 
 logger = logging.getLogger(__name__)
 
@@ -74,7 +74,7 @@ def get_gcp_metadata() -> Optional[GcpInstanceMetadata]:
         headers={"Metadata-Flavor": "Google"},
     )
     if response is None:
-        return response
+        return None
     instance_metadata = response.json()
     availability_zone = instance_metadata["zone"]
     instance_type = instance_metadata["machineType"]
@@ -138,7 +138,7 @@ def get_azure_metadata() -> Optional[AzureInstanceMetadata]:
 
 
 def send_request(url: str, headers: Dict[str, str] = None) -> Optional[Response]:
-    response = requests.get(url, headers=headers or {}, timeout=AWS_TIMEOUT)
+    response = requests.get(url, headers=headers or {}, timeout=METADATA_REQUEST_TIMEOUT)
     if not response.ok:
         raise BadResponseCode(response.status_code)
     return response
