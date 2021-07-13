d7188a124bbdf3cedcce0b289a06834c214fc86e
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2021-07-13 12:18:39 +0300

    Update async-profiler tag

--- scripts/async_profiler_build.sh
+++ scripts/async_profiler_build.sh
@@ -5,5 +5,5 @@
 #
 set -euo pipefail
 
-git clone --depth 1 -b static-libstdcpp https://github.com/Granulate/async-profiler.git && cd async-profiler && git reset --hard 9d29169e34abc004f534a85ba6a4cf8920250381
+git clone --depth 1 -b v2.0g4 https://github.com/Granulate/async-profiler.git && cd async-profiler && git reset --hard 9d29169e34abc004f534a85ba6a4cf8920250381
 make release
