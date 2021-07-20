f5d4229a69535359fb82434371023f1b7a7b7b14
MichelOS <michel.heily@granulate.io>
2021-07-20 18:05:14 +0300

    Add missing licenses to source code (#146)
    * Add missing licenses to source code

--- gprofiler/state.py
+++ gprofiler/state.py
@@ -1,3 +1,8 @@
+#
+# Copyright (c) Granulate. All rights reserved.
+# Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
+#
+
 import uuid
 from typing import Optional
 
