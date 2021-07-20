9c11cc7001717b39c9b97700111ef21de76448c5
Yishai Zinkin <yishai.zinkin@granulate.io>
2021-07-20 11:26:26 +0000

    Remove libunwind build

--- scripts/pyperf_env.sh
+++ scripts/pyperf_env.sh
@@ -9,13 +9,13 @@ apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y curl git build-essential iperf llvm-9-dev libclang-9-dev \
   cmake python3 flex bison libelf-dev libz-dev
 
-cd /tmp
+# cd /tmp
 
-# Install libunwind
-curl -L http://download.savannah.nongnu.org/releases/libunwind/libunwind-1.4.0.tar.gz -o libunwind-1.4.0.tar.gz
-tar -xf libunwind-1.4.0.tar.gz
-pushd libunwind-1.4.0
-./configure --prefix=/usr && make install
-popd
-rm -r libunwind-1.4.0
-rm libunwind-1.4.0.tar.gz
+# # Install libunwind
+# curl -L http://download.savannah.nongnu.org/releases/libunwind/libunwind-1.4.0.tar.gz -o libunwind-1.4.0.tar.gz
+# tar -xf libunwind-1.4.0.tar.gz
+# pushd libunwind-1.4.0
+# ./configure --prefix=/usr && make install
+# popd
+# rm -r libunwind-1.4.0
+# rm libunwind-1.4.0.tar.gz
