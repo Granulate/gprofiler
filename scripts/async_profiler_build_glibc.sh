8779dfb54146570fd13da5b98a5abf496ac10380
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2021-11-11 10:35:57 +0200

    java: Build AP with musl and select it when needed

---
+++ scripts/async_profiler_build_glibc.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) Granulate. All rights reserved.
+# Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
+#
+set -euo pipefail
+
+# This script is invoked by async_profiler_build_shared.sh
+set +eu  # this funny script has errors :shrug:
+source scl_source enable devtoolset-7
+set -eu
--- scripts/async_profiler_build_shared.sh
+++ scripts/async_profiler_build_shared.sh
@@ -9,9 +9,7 @@ VERSION=v2.5g1
 GIT_REV="02cb785f7cc0024581a1802126f51ed94d5b0475"
 
 git clone --depth 1 -b "$VERSION" https://github.com/Granulate/async-profiler.git && cd async-profiler && git reset --hard "$GIT_REV"
-set +eu  # this funny script has errors :shrug:
-source scl_source enable devtoolset-7
-set -eu
+source "$1"
 make all
 
 # add a version file to the build directory
