20106d6b8d7ff7049d8ff8bfe2ee42694d6ad73d
David Dorfman <david.dorfman@granulate.io>
2021-11-23 18:53:17 +0200

    lint

--- gprofiler/devkmsg.py
+++ gprofiler/devkmsg.py
@@ -7,6 +7,7 @@ import os
 import time
 from typing import List, Tuple
 
+from gprofiler.kernel_messages import Provider
 from gprofiler.log import get_logger_adapter
 
 logger = get_logger_adapter(__name__)
@@ -15,7 +16,7 @@ logger = get_logger_adapter(__name__)
 CONSOLE_EXT_LOG_MAX = 8192
 
 
-class DevKmsgProvider:
+class DevKmsgProvider(Provider):
     # The /dev/kmsg interfaced is described at Documentation/ABI/testing/dev-kmsg in the kernel source tree
     # and can be viewed at https://github.com/torvalds/linux/blob/master/Documentation/ABI/testing/dev-kmsg.
     def __init__(self):
--- gprofiler/kernel_messages.py
+++ gprofiler/kernel_messages.py
@@ -2,14 +2,24 @@
 # Copyright (c) Granulate. All rights reserved.
 # Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
 #
+from abc import ABC
+from typing import Iterable, Tuple, Type
+
 from gprofiler.log import get_logger_adapter
 from gprofiler.utils import get_kernel_release
 
 logger = get_logger_adapter(__name__)
 kernel_release = get_kernel_release()
 
+KernelMessage = Tuple[float, int, str]
 
-class EmptyProvider:
+
+class Provider(ABC):
+    def iter_new_messages(self) -> Iterable[KernelMessage]:
+        ...
+
+
+class EmptyProvider(Provider):
     def __init__(self):
         print("This kernel does not support the new /dev/kmsg interface for reading messages.")
         print("Profilee error monitoring not available.")
@@ -20,6 +30,7 @@ class EmptyProvider:
         return []
 
 
+DefaultMessagesProvider: Type[Provider]
 if kernel_release >= (3, 5):
     from gprofiler.devkmsg import DevKmsgProvider
 
