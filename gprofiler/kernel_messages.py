ef4dd572b8e7cbff11c9189757f84b09d01b06a4
David Dorfman <david.dorfman@granulate.io>
2021-11-29 13:41:09 +0200

    remove prints and kernel-version specific log

--- gprofiler/kernel_messages.py
+++ gprofiler/kernel_messages.py
@@ -2,7 +2,6 @@
 # Copyright (c) Granulate. All rights reserved.
 # Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
 #
-from granulate_utils.linux import get_kernel_release
 from granulate_utils.linux.kernel_messages import DefaultKernelMessagesProvider, EmptyKernelMessagesProvider
 
 from gprofiler.log import get_logger_adapter
@@ -16,18 +15,12 @@ class GProfilerKernelMessagesProvider(DefaultKernelMessagesProvider):
 
 
 def get_kernel_messages_provider():
-    if get_kernel_release() < (3, 5):
-        print("This kernel does not support the new /dev/kmsg interface for reading messages.")
-        print("Profilee error monitoring not available.")
-        print()
-        logger.warning("Profilee error monitoring not available.")
-
     try:
         return GProfilerKernelMessagesProvider()
     except Exception:
-        print("Failed to start kernel messages listener. Do you have permission to read /dev/kmsg ?")
-        print("Profilee error monitoring not available.")
-        print()
-        logger.warning("Failed to start kernel messages listener.", exc_info=True)
-        logger.warning("Profilee error monitoring not available.")
+        logger.warning(
+            "Failed to start kernel messages listener. Profilee error monitoring not available. (Do you have permission"
+            " to read /dev/kmsg?)",
+            exc_info=True,
+        )
         return EmptyKernelMessagesProvider()
