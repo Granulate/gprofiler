a4f8b2aa84cfe42156cf4d33a72cf243351fe70d
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2022-01-13 23:27:49 +0200

    utils: Fix can_i_use_perf_events() to our perf version, and add a comment

--- gprofiler/utils.py
+++ gprofiler/utils.py
@@ -496,11 +496,16 @@ def perf_path() -> str:
 
 def can_i_use_perf_events() -> bool:
     # checks access to perf_events
+    # TODO invoking perf has a toll of about 1 second on my box; maybe we want to directly call
+    # perf_event_open here for this test?
     try:
         run_process([perf_path(), "record", "-o", "/dev/null", "--", "/bin/true"])
     except CalledProcessError as e:
         # perf's output upon start error (e.g due to permissions denied error)
-        if e.returncode == 255 and "perf_event_open(..., 0) failed unexpectedly with error" in e.stderr:
+        if (
+            e.returncode == 255
+            and b"Access to performance monitoring and observability operations is limited" in e.stderr
+        ):
             return False
         raise
     else:
