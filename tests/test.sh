4fe271d11a2e4315e80552ab31147a38ffa99cb6
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2022-01-15 17:57:46 +0200

    PATCH ci test

--- tests/test.sh
+++ tests/test.sh
@@ -7,6 +7,9 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"  # https://stackoverflow.com/a/246128
 
+sudo DEBIAN_FRONTEND=noninteractive apt-get -qq update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -qq --no-install-recommends libc6
+
 if [ -z ${NO_APT_INSTALL+x} ]; then
  sudo DEBIAN_FRONTEND=noninteractive apt-get -qq update
  sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -qq --no-install-recommends openjdk-8-jdk python3 python3-pip docker.io php ruby2.7
