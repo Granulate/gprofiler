c5937a6e1181ad91f195a0d3ac8fa66a89a4f0db
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2022-02-01 01:23:06 +0200

    wip update bpf helpers to aarch64 revisions

--- scripts/bcc_helpers_build.sh
+++ scripts/bcc_helpers_build.sh
@@ -5,14 +5,6 @@
 #
 set -euo pipefail
 
-# TODO support aarch64, after we support it in PyPerf
-if [ $(uname -m) != "x86_64" ]; then
-    mkdir -p /bpf_get_fs_offset /bpf_get_stack_offset
-    touch /bpf_get_fs_offset/get_fs_offset
-    touch /bpf_get_stack_offset/get_stack_offset
-    exit 0
-fi
-
 LLVM_STRIP=llvm-strip
 if ! command -v "$LLVM_STRIP" > /dev/null 2>&1 ; then
     LLVM_STRIP=llvm-strip-10
@@ -20,10 +12,10 @@ fi
 
 LIBBPF_MAKE_FLAGS="BPFTOOL=/bpftool CLANG=clang-10 LLVM_STRIP=$LLVM_STRIP CFLAGS=-static"
 
-cd / && git clone -b v0.0.2 --depth=1 --recurse-submodules https://github.com/Jongy/bpf_get_fs_offset.git
-cd /bpf_get_fs_offset && git reset --hard 8326d39cf44845d4b643ed4267994afca8ccecb3
+cd / && git clone -b aarch64 --depth=1 --recurse-submodules https://github.com/Jongy/bpf_get_fs_offset.git
+cd /bpf_get_fs_offset && git reset --hard 094e93f979308d46dffb8d4ea88823f68d53ba85
 cd /bpf_get_fs_offset && make $LIBBPF_MAKE_FLAGS
 
-cd / && git clone -b v0.0.3 --depth=1 --recurse-submodules https://github.com/Jongy/bpf_get_stack_offset.git
-cd /bpf_get_stack_offset && git reset --hard 54b70ee65708cc8d3d7817277e82376d95205356
+cd / && git clone -b aarch64 --depth=1 --recurse-submodules https://github.com/Jongy/bpf_get_stack_offset.git
+cd /bpf_get_stack_offset && git reset --hard d8b77ce6da674c38ad0bb856686fde1e63ad0814
 cd /bpf_get_stack_offset && make $LIBBPF_MAKE_FLAGS
