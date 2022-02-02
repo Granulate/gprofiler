69dbff090c1e6675a17adc568ae9977accfcf377
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2022-02-03 01:25:38 +0200

    wip

--- scripts/pyperf_build.sh
+++ scripts/pyperf_build.sh
@@ -5,9 +5,10 @@
 #
 set -e
 
-git clone --depth 1 -b aarch64 https://github.com/Granulate/bcc.git && cd bcc && git reset --hard 91a4accb4c114a6b2185c1622b92437f16a09acc
+git clone --depth 1 -b aarch64 https://github.com/Granulate/bcc.git && cd bcc && git reset --hard aba6bdc0dcf089128b5d74f4b30ee0d86b56567b
 
 mkdir build
 cd build
+# TODO -DENABLE_LLVM_SHARED only for aarch exe
 cmake -DPYTHON_CMD=python3 -DINSTALL_CPP_EXAMPLES=y -DCMAKE_INSTALL_PREFIX=/bcc/root -DENABLE_LLVM_SHARED=1 ..
 make -C examples/cpp/pyperf -j -l VERBOSE=1 install
