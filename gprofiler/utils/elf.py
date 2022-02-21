e0e845869e3cd2cc70ead703180de87c245f898c
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2022-02-22 01:48:46 +0200

    utils: elf: Add get_process_execfn()

--- gprofiler/utils/elf.py
+++ gprofiler/utils/elf.py
@@ -3,10 +3,12 @@
 # Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
 #
 
+import struct
 from typing import Optional, cast
 
 from elftools.elf.elffile import ELFFile  # type: ignore
 from elftools.elf.sections import NoteSection  # type: ignore
+from psutil import NoSuchProcess, Process
 
 
 def get_elf_buildid(path: str) -> Optional[str]:
@@ -25,3 +27,43 @@ def get_elf_buildid(path: str) -> Optional[str]:
                 return cast(str, note.n_desc)
         else:
             return None
+
+
+_AUXV_ENTRY = struct.Struct("LL")
+
+AT_EXECFN = 31
+PATH_MAX = 4096
+
+
+def _get_process_auxv(process: Process, auxv_id: int) -> int:
+    try:
+        with open(f"/proc/{process.pid}/auxv", "rb") as f:
+            auxv = f.read()
+    except FileNotFoundError:
+        raise NoSuchProcess(process.pid)
+
+    for i in range(0, len(auxv), _AUXV_ENTRY.size):
+        entry = auxv[i : i + _AUXV_ENTRY.size]
+        id_, val = _AUXV_ENTRY.unpack(entry)
+
+        if id_ == auxv_id:
+            assert isinstance(val, int)  # mypy fails to understand
+            return val
+    else:
+        raise ValueError(f"auxv id {auxv_id} was not found!")
+
+
+def _read_process_memory(process: Process, addr: int, size: int) -> bytes:
+    try:
+        with open(f"/proc/{process.pid}/mem", "rb", buffering=0) as mem:
+            mem.seek(addr)
+            return mem.read(size)
+    except FileNotFoundError:
+        raise NoSuchProcess(process.pid)
+
+
+def get_process_execfn(process: Process) -> str:
+    # reads process AT_EXECFN
+    addr = _get_process_auxv(process, AT_EXECFN)
+    fn = _read_process_memory(process, addr, PATH_MAX)
+    return fn[: fn.index(b"\0")].decode()
