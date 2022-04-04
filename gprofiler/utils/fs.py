eaea960e9f5eb6ad869a470812d369b8644d3577
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2022-04-04 10:48:25 +0300

    java: Select process AP dir based on rw,exec one

--- gprofiler/utils/fs.py
+++ gprofiler/utils/fs.py
@@ -3,8 +3,11 @@
 # Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
 #
 
+import errno
 import os
 import shutil
+import subprocess
+from pathlib import Path
 
 
 def safe_copy(src: str, dst: str) -> None:
@@ -15,3 +18,31 @@ def safe_copy(src: str, dst: str) -> None:
     dst_tmp = f"{dst}.tmp"
     shutil.copy(src, dst_tmp)
     os.rename(dst_tmp, dst)
+
+
+def is_rw_exec_dir(path: str) -> bool:
+    """
+    Is 'path' rw and exec?
+    """
+    test_script = Path(path) / "t.sh"
+
+    # try creating & writing
+    try:
+        os.makedirs(path, 0o755, exist_ok=True)
+        test_script.write_text("#!/bin/sh\nexit 0")
+        test_script.chmod(0o755)
+    except OSError as e:
+        if e.errno == errno.EROFS:
+            # ro
+            return False
+
+    # try executing
+    try:
+        subprocess.run(str(test_script), check=True)
+    except PermissionError:
+        # noexec
+        return False
+    finally:
+        test_script.unlink()
+
+    return True
