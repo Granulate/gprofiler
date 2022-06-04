04291a9f6e62cbfca36310c718db33427688b45c
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2022-06-04 03:54:30 +0300

    perf: Upgrade elftuils to 0.187 and disable debuginfod properly
    elfutils commit 4ced3010ad11 prevents libdw from calling the unwanted dlopen(),
    and it wasn't a part of 0.179. So I upgraded to latest.

--- scripts/perf_env.sh
+++ scripts/perf_env.sh
@@ -38,10 +38,12 @@ apt-get install -y --no-install-recommends \
 cd /tmp
 
 # install newer versions of elfutils
-curl -L ftp://sourceware.org/pub/elfutils/0.179/elfutils-0.179.tar.bz2 -o elfutils-0.179.tar.bz2
-tar -xf elfutils-0.179.tar.bz2
-pushd elfutils-0.179
-./configure --disable-debuginfod --prefix=/usr && make -j 8 && make install
+curl -L ftp://sourceware.org/pub/elfutils/0.187/elfutils-0.187.tar.bz2 -o elfutils-0.187.tar.bz2
+tar -xf elfutils-0.187.tar.bz2
+pushd elfutils-0.187
+# disable debuginfod, otherwise it will try to dlopen("libdebuginfod.so") in runtime & that can
+# cause problems, see https://github.com/Granulate/gprofiler/issues/340.
+./configure --disable-debuginfod --disable-libdebuginfod --prefix=/usr && make -j 8 && make install
 popd
-rm -r elfutils-0.179
-rm elfutils-0.179.tar.bz2
+rm -r elfutils-0.187
+rm elfutils-0.187.tar.bz2
