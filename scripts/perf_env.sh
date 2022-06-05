7811a4490c9e659333f06204a5fd107b9a937a71
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2022-06-05 16:05:46 +0300

    Build & install libzstd

--- scripts/perf_env.sh
+++ scripts/perf_env.sh
@@ -30,12 +30,19 @@ apt-get install -y --no-install-recommends \
     libbabeltrace-ctf-dev \
     systemtap-sdt-dev \
     libslang2-dev \
-    libzstd-dev \
     libbz2-dev \
     flex \
     bison
 
 cd /tmp
+
+# build & install libzstd (elfutils requires newer versions than available in apt)
+curl -L https://github.com/facebook/zstd/releases/download/v1.5.2/zstd-1.5.2.tar.gz -o zstd-1.5.2.tar.gz
+tar -xf zstd-1.5.2.tar.gz
+pushd zstd-1.5.2
+make -j && make install
+popd
+rm -r zstd-1.5.2 zstd-1.5.2.tar.gz
 
 # install newer versions of elfutils
 curl -L ftp://sourceware.org/pub/elfutils/0.187/elfutils-0.187.tar.bz2 -o elfutils-0.187.tar.bz2
@@ -45,5 +52,4 @@ pushd elfutils-0.187
 # cause problems, see https://github.com/Granulate/gprofiler/issues/340.
 ./configure --disable-debuginfod --disable-libdebuginfod --prefix=/usr && make -j 8 && make install
 popd
-rm -r elfutils-0.187
-rm elfutils-0.187.tar.bz2
+rm -r elfutils-0.187 elfutils-0.187.tar.bz2
