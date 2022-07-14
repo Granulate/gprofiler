1c4e20f2b13252a3f864b61dd6f46b06d356df39
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2022-07-14 03:01:04 +0300

    java: Upgrade AP v2.8.2

--- scripts/async_profiler_build_shared.sh
+++ scripts/async_profiler_build_shared.sh
@@ -5,8 +5,8 @@
 #
 set -euo pipefail
 
-VERSION=v2.8.1g2
-GIT_REV="83fd51841ab368b0c89e20320a6d45e31a1bec5d"
+VERSION=v2.8.2g1
+GIT_REV="edd7a931fd645591bf8784b8383aeaecaa1fc556"
 
 git clone --depth 1 -b "$VERSION" https://github.com/Granulate/async-profiler.git && cd async-profiler && git reset --hard "$GIT_REV"
 source "$1"
