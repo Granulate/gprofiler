b68ce9bb0cbd966aee72ec2f26b7cba83d6b15c7
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2022-07-26 03:49:26 +0300

    Add PoC speedscope -> collapsed converter

---
+++ gprofiler/utils/speedscope.py
@@ -0,0 +1,63 @@
+#
+# Copyright (c) Granulate. All rights reserved.
+# Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
+#
+
+# speedscope -> collapsed converter, aimed to work for dotnet-trace.
+# speedscope spec: https://github.com/jlfwong/speedscope/blob/639dae322b15fbcba5cd02c90335889fd285686a/src/lib/file-format-spec.ts
+
+import json
+import math
+import random
+from collections import Counter
+from typing import Any, Dict, List, Tuple
+
+from gprofiler.gprofiler_types import StackToSampleCount
+
+
+def _speedscope_frame_name(speedscope: Dict[str, Any], frame: int) -> str:
+    name = speedscope["shared"]["frames"][frame]["name"]
+    assert isinstance(name, str)
+    return name
+
+
+def load_speedscope_as_collapsed(speedscope_path: str, frequncy_hz: int) -> StackToSampleCount:
+    interval = 1 / frequncy_hz
+    interval_ms = interval * 1000
+
+    with open(speedscope_path) as f:
+        speedscope = json.load(f)
+
+    result_stacks: StackToSampleCount = Counter()
+
+    for profile in speedscope["profiles"]:  # a profile per thread
+        assert profile["type"] == "evented", profile["type"]  # what dotnet-trace uses
+        assert profile["unit"] == "milliseconds", profile["unit"]  # what dotnet-trace uses
+        stack: List[int] = []
+        stacks: List[Tuple[int, ...]] = []
+
+        # needs to be a float, but dotnet-trace puts a string...
+        last_ts = float(profile["startValue"])  # matches the ts of first event
+        for event in profile["events"]:
+            at = event["at"]
+            elapsed = at - last_ts
+            last_ts = at
+
+            if event["type"] == "O":
+                stack.append(event["frame"])
+            else:
+                assert event["type"] == "C", f"unexpected event type: {event['type']}"
+                assert stack[-1] == event["frame"]
+                stack.pop()
+
+            frac, n = math.modf(elapsed / interval_ms)
+            assert int(n) == n
+            for _ in range(int(n)):
+                stacks.append(tuple(stack))
+            if random.random() <= frac:
+                stacks.append(tuple(stack))
+
+        for s in stacks:
+            result_stacks[";".join(map(lambda f: _speedscope_frame_name(speedscope, f), s))] += 1
+
+    return result_stacks
