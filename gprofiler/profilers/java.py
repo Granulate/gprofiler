c4a06cfbee717b8eb94b93bb5d20e073c038cca3
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2022-08-08 12:18:11 +0300

    fix imports

--- gprofiler/profilers/java.py
+++ gprofiler/profilers/java.py
@@ -27,11 +27,10 @@ from granulate_utils.java import (
     locate_hotspot_error_file,
 )
 from granulate_utils.linux import proc_events
-from granulate_utils.linux.elf import get_mapped_dso_elf_id
 from granulate_utils.linux.kernel_messages import KernelMessage
 from granulate_utils.linux.ns import get_proc_root_path, get_process_nspid, resolve_proc_root_links, run_in_ns
 from granulate_utils.linux.oom import get_oom_entry
-from granulate_utils.linux.process import is_musl, is_process_running, process_exe
+from granulate_utils.linux.process import get_mapped_dso_elf_id, is_musl, is_process_running, process_exe
 from granulate_utils.linux.signals import get_signal_entry
 from packaging.version import Version
 from psutil import Process
