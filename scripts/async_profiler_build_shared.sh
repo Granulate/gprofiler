9702a7a911e652299ba070c58a226690131e192b
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2022-08-31 12:15:18 +0300

    remove supposedly flaky checks

--- scripts/async_profiler_build_shared.sh
+++ scripts/async_profiler_build_shared.sh
@@ -14,18 +14,3 @@ make all
 
 # add a version file to the build directory
 echo -n "$VERSION" > build/async-profiler-version
-
-# build verifications regarding libstdc++.
-if ldd /bin/ls 2>&1 | grep -q musl ; then
-    # ensure no libstdc++
-    if ldd build/libasyncProfiler.so 2>&1 | tee /dev/fd/2 | grep -q "libstdc++"; then
-        echo "libstdc++ found!"
-        exit 1
-    fi
-else
-    # ensure libstdc++
-    if ! ldd build/libasyncProfiler.so 2>&1 | tee /dev/fd/2 | grep -q "libstdc++"; then
-        echo "libstdc++ not found!"
-        exit 1
-    fi
-fi
