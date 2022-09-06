d77b56cb155ebfc70d3b894eba11b3835c318292
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2022-09-07 00:44:18 +0300

    Fix lint stuff

--- scripts/dotnet_prepare_dependencies.sh
+++ scripts/dotnet_prepare_dependencies.sh
@@ -3,11 +3,11 @@
 # Copyright (c) Granulate. All rights reserved.
 # Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
 #
-# set -euo pipefail
+set -euo pipefail
 
 declare -a deps=("libclrjit.so"
                  "libhostpolicy.so"
-                 "libcoreclr.so" 
+                 "libcoreclr.so"
                  "libSystem.Native.so"
                  "Microsoft.CSharp.dll"
                  "Microsoft.NETCore.App.deps.json"
@@ -34,22 +34,22 @@ declare -a deps=("libclrjit.so"
                  "System.Linq.Expressions.dll"
                  "System.Memory.dll"
                  "System.Private.CoreLib.dll"
-                 "System.Private.Uri.dll" 
+                 "System.Private.Uri.dll"
                  "System.Runtime.dll"
                  "System.Runtime.Extensions.dll"
-                 "System.Runtime.InteropServices.dll" 
-                 "System.Runtime.InteropServices.RuntimeInformation.dll" 
+                 "System.Runtime.InteropServices.dll"
+                 "System.Runtime.InteropServices.RuntimeInformation.dll"
                  "System.Text.RegularExpressions.dll"
-                 "System.Threading.Channels.dll" 
+                 "System.Threading.Channels.dll"
                  "System.Threading.dll"
-                 "System.Threading.Overlapped.dll" 
-                 "System.Threading.Tasks.dll" 
+                 "System.Threading.Overlapped.dll"
+                 "System.Threading.Tasks.dll"
                  "System.Threading.Thread.dll"
                  "System.Threading.ThreadPool.dll"
-                 "System.IO.Pipes.dll" 
-                 "System.Net.Sockets.dll" 
-                 "System.Net.Primitives.dll" 
-                 "System.Security.Principal.dll" 
+                 "System.IO.Pipes.dll"
+                 "System.Net.Sockets.dll"
+                 "System.Net.Primitives.dll"
+                 "System.Security.Principal.dll"
                  "System.IO.dll"
                  "System.Security.Cryptography.Algorithms.dll"
                  "System.Security.Cryptography.Primitives.dll"
@@ -65,5 +65,3 @@ for i in "${deps[@]}"
 do
    cp "/usr/share/dotnet/shared/Microsoft.NETCore.App/6.0.7/$i" "/tmp/dotnet/deps/$i"
 done
-
- 
