f69f6d58bcb45bba19ad560797b7f755718da964
Temitope Oluwafemi <temitope.e.oluwafemi@intel.com>
2022-09-26 18:45:54 -0400

    Removed SystemProfiler from windows imports

--- gprofiler/profilers/__init__.py
+++ gprofiler/profilers/__init__.py
@@ -1,11 +1,11 @@
 # NOTE: Make sure to import any new process profilers to load it
 from gprofiler.platform import is_windows
-from gprofiler.profilers.perf import SystemProfiler
 from gprofiler.profilers.python import PythonProfiler
 
 if not is_windows():
     from gprofiler.profilers.dotnet import DotnetProfiler
     from gprofiler.profilers.java import JavaProfiler
+    from gprofiler.profilers.perf import SystemProfiler
     from gprofiler.profilers.php import PHPSpyProfiler
     from gprofiler.profilers.ruby import RbSpyProfiler
 
