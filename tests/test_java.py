d58b971d75ed3de43ac7eb82811a1ec38fd916bd
Marcin Poźniak <marcin.pozniak@intel.com>
2022-10-06 14:15:27 +0200

    Remove unused import

--- tests/test_java.py
+++ tests/test_java.py
@@ -2,7 +2,6 @@
 # Copyright (c) Granulate. All rights reserved.
 # Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
 #
-from email.mime import application
 import logging
 import os
 import shutil
