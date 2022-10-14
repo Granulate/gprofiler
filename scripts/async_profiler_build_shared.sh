1d8a5e79f670aac1bf3260878180729a11ddc2dc
Marcin Olszewski <marcin.olszewski@intel.com>
2022-10-14 15:03:13 +0200

    Fix test_sanity_other_jvms

--- scripts/async_profiler_build_shared.sh
+++ scripts/async_profiler_build_shared.sh
@@ -6,7 +6,7 @@
 set -euo pipefail
 
 VERSION=apply-common-format-for-DSO-names
-GIT_REV="d1c6e4aa5ac12102ae1336a2a96a5c4a385ad9f3"
+GIT_REV="67e08722a2387dfd8d17f1d844d6efece2a36e6a"
 
 git clone --depth 1 -b "$VERSION" https://github.com/marcin-ol/async-profiler.git && cd async-profiler && git reset --hard "$GIT_REV"
 # shellcheck disable=SC1090  # we pass it either async_profiler_build_glibc.sh or async_profiler_build_musl.sh
