2d4de389ee2106b449dab013ae4a9845437b9e99
Ariel Moshe <ariel.moshe@granulate.io>
2022-10-14 10:59:14 +0300

    Update granulate-utils (#483)
    This PR updates granulate-utils post the changes in https://github.com/Granulate/granulate-utils/pull/73,
    and includes a few other updates from the utils repo as well, one of them is https://github.com/Granulate/granulate-utils/pull/87 which fixes https://github.com/Granulate/gprofiler/issues/503.

--- gprofiler/utils/process.py
+++ gprofiler/utils/process.py
@@ -3,12 +3,10 @@
 # Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
 #
 
-import os
 import re
-from functools import lru_cache
 from typing import Match, Optional
 
-from granulate_utils.linux.process import process_exe, read_proc_file
+from granulate_utils.linux.process import read_proc_file
 from psutil import Process
 
 
@@ -21,16 +19,3 @@ def process_comm(process: Process) -> str:
     name_line = status.splitlines()[0]
     assert name_line.startswith("Name:\t")
     return name_line.split("\t", 1)[1]
-
-
-@lru_cache(maxsize=512)
-def is_process_basename_matching(process: Process, basename_pattern: str) -> bool:
-    if re.match(basename_pattern, os.path.basename(process_exe(process))):
-        return True
-
-    # process was executed AS basename (but has different exe name)
-    cmd = process.cmdline()
-    if len(cmd) > 0 and re.match(basename_pattern, cmd[0]):
-        return True
-
-    return False
