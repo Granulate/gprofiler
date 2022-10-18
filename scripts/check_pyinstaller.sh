59f85a9a22a4a9218f307332244c5f638f909998
teoluwaf <temitope.e.oluwafemi@intel.com>
2022-10-18 19:44:02 -0400

    Prepare metadata & utils code for Windows support (#515)
    Co-authored-by: Yonatan Goldschmidt <yon.goldschmidt@gmail.com>

---
+++ scripts/check_pyinstaller.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) Granulate. All rights reserved.
+# Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
+#
+set -uo pipefail
+
+# grep returns 0 if a match is found and 1 if no match is found
+result=$(grep "gprofiler\." "build/pyinstaller/warn-pyinstaller.txt" | grep -v 'missing module named wmi' | grep -v 'missing module named pythoncom' | grep -v 'missing module named netifaces')
+
+if [ -n "$result" ]; then
+	echo 'PyInstaller failed to pack gProfiler code! See lines above. Make sure to check for SyntaxError as this is often the reason.';
+	exit 1;
+fi
