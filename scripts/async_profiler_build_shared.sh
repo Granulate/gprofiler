b4d17b07f7098ad1d9d5e6364ab6bdf5dd74cdaf
Marcin Olszewski <marcin.olszewski@intel.com>
2022-10-24 17:38:38 +0200

    Apply latest changes to async-profiler

--- scripts/async_profiler_build_shared.sh
+++ scripts/async_profiler_build_shared.sh
@@ -6,7 +6,7 @@
 set -euo pipefail
 
 VERSION=apply-common-format-for-DSO-names
-GIT_REV="f7aabd7aca25fe8c7ee0411ccdd08b6ab97f694d"
+GIT_REV="a3b4174e2af6a4e4c63de55c603ea0ca5d93def9"
 
 git clone --depth 1 -b "$VERSION" https://github.com/marcin-ol/async-profiler.git && cd async-profiler && git reset --hard "$GIT_REV"
 # shellcheck disable=SC1090  # we pass it either async_profiler_build_glibc.sh or async_profiler_build_musl.sh
