37062c08b079f85dc1e0f2689ce5c2ca78902f15
Marcin Poźniak <marcin.pozniak@intel.com>
2022-10-24 12:50:20 +0200

    Add xfail to node12-musl test

--- tests/test_node.py
+++ tests/test_node.py
@@ -99,8 +99,8 @@ def test_nodejs_matrix(
     profiler_flags: List[str],
     application_image_tag: str,
 ) -> None:
-    node_version = application_image_tag.split("-")[0]
-    if node_version in ["14", "15", "16"]:
+    node_version, libc = application_image_tag.split("-")
+    if node_version in ["14", "15", "16"] or (node_version == "12" and libc == "musl"):
         pytest.xfail("This test fails with these nodejs versions, see https://github.com/Granulate/gprofiler/issues/550")
     with SystemProfiler(
         1000,
