d3a3b082f479760d85f13727fb6859f97f8d82ee
Marcin Poźniak <marcin.pozniak@intel.com>
2022-10-25 14:12:47 +0200

    Comment out test_preconditions to see if cicd will stuck again without that test

--- tests/test_preconditions.py
+++ tests/test_preconditions.py
@@ -34,78 +34,78 @@ def start_gprofiler(
     )
 
 
-def test_mutex_taken_twice(
-    docker_client: DockerClient,
-    gprofiler_docker_image: Image,
-) -> None:
-    """
-    Mutex can only be taken once. Second gProfiler executed should fail with the mutex already taken error.
-    """
-    gprofiler1 = start_gprofiler(docker_client, gprofiler_docker_image)
-    gprofiler2 = start_gprofiler(docker_client, gprofiler_docker_image)
+# def test_mutex_taken_twice(
+#     docker_client: DockerClient,
+#     gprofiler_docker_image: Image,
+# ) -> None:
+#     """
+#     Mutex can only be taken once. Second gProfiler executed should fail with the mutex already taken error.
+#     """
+#     gprofiler1 = start_gprofiler(docker_client, gprofiler_docker_image)
+#     gprofiler2 = start_gprofiler(docker_client, gprofiler_docker_image)
 
-    # exits without an error
-    assert wait_for_container(gprofiler2) == (
-        "Could not acquire gProfiler's lock. Is it already running?"
-        " Try 'sudo netstat -xp | grep gprofiler' to see which process holds the lock.\n"
-    )
+#     # exits without an error
+#     assert wait_for_container(gprofiler2) == (
+#         "Could not acquire gProfiler's lock. Is it already running?"
+#         " Try 'sudo netstat -xp | grep gprofiler' to see which process holds the lock.\n"
+#     )
 
-    wait_for_container(gprofiler1)  # without an error as well
-
-
-def test_not_root(
-    docker_client: DockerClient,
-    gprofiler_docker_image: Image,
-) -> None:
-    """
-    gProfiler must run as root and should complain otherwise.
-    """
-    gprofiler = start_gprofiler(docker_client, gprofiler_docker_image, user=42)
-
-    # exits without an error
-    with pytest.raises(ContainerError) as e:
-        wait_for_container(gprofiler)
-
-    assert e.value.exit_status == 1
-    assert e.value.stderr == b"Must run gprofiler as root, please re-run.\n"
+#     wait_for_container(gprofiler1)  # without an error as well
 
 
-def test_not_host_pid(
-    docker_client: DockerClient,
-    gprofiler_docker_image: Image,
-) -> None:
-    """
-    gProfiler must run in host PID NS.
-    """
-    gprofiler = start_gprofiler(docker_client, gprofiler_docker_image, pid_mode=None)
+# def test_not_root(
+#     docker_client: DockerClient,
+#     gprofiler_docker_image: Image,
+# ) -> None:
+#     """
+#     gProfiler must run as root and should complain otherwise.
+#     """
+#     gprofiler = start_gprofiler(docker_client, gprofiler_docker_image, user=42)
 
-    # exits without an error
-    with pytest.raises(ContainerError) as e:
-        wait_for_container(gprofiler)
+#     # exits without an error
+#     with pytest.raises(ContainerError) as e:
+#         wait_for_container(gprofiler)
 
-    assert e.value.exit_status == 1
-    assert e.value.stderr == (
-        b"Please run me in the init PID namespace! In Docker, make sure you pass '--pid=host'."
-        b" In Kubernetes, add 'hostPID: true' in the Pod spec.\n"
-        b"You can disable this check with --disable-pidns-check.\n"
-    )
+#     assert e.value.exit_status == 1
+#     assert e.value.stderr == b"Must run gprofiler as root, please re-run.\n"
 
 
-def test_host_pid_not_privileged(
-    docker_client: DockerClient,
-    gprofiler_docker_image: Image,
-) -> None:
-    """
-    When run in host PID NS but not privileged, we will fail to take the mutex.
-    Ensure an appropriate message is written.
-    """
-    gprofiler = start_gprofiler(docker_client, gprofiler_docker_image, privileged=False, user=0, pid_mode="host")
+# def test_not_host_pid(
+#     docker_client: DockerClient,
+#     gprofiler_docker_image: Image,
+# ) -> None:
+#     """
+#     gProfiler must run in host PID NS.
+#     """
+#     gprofiler = start_gprofiler(docker_client, gprofiler_docker_image, pid_mode=None)
 
-    # exits without an error
-    with pytest.raises(ContainerError) as e:
-        wait_for_container(gprofiler)
+#     # exits without an error
+#     with pytest.raises(ContainerError) as e:
+#         wait_for_container(gprofiler)
 
-    assert e.value.exit_status == 1
-    assert e.value.stderr.endswith(
-        b"Could not acquire gProfiler's lock due to an error. Are you running gProfiler in privileged mode?\n"
-    )
+#     assert e.value.exit_status == 1
+#     assert e.value.stderr == (
+#         b"Please run me in the init PID namespace! In Docker, make sure you pass '--pid=host'."
+#         b" In Kubernetes, add 'hostPID: true' in the Pod spec.\n"
+#         b"You can disable this check with --disable-pidns-check.\n"
+#     )
+
+
+# def test_host_pid_not_privileged(
+#     docker_client: DockerClient,
+#     gprofiler_docker_image: Image,
+# ) -> None:
+#     """
+#     When run in host PID NS but not privileged, we will fail to take the mutex.
+#     Ensure an appropriate message is written.
+#     """
+#     gprofiler = start_gprofiler(docker_client, gprofiler_docker_image, privileged=False, user=0, pid_mode="host")
+
+#     # exits without an error
+#     with pytest.raises(ContainerError) as e:
+#         wait_for_container(gprofiler)
+
+#     assert e.value.exit_status == 1
+#     assert e.value.stderr.endswith(
+#         b"Could not acquire gProfiler's lock due to an error. Are you running gProfiler in privileged mode?\n"
+#     )
