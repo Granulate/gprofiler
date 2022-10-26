47ae4736e14e93225012a99a5c0f761c1109055c
Marcin Poźniak <marcin.pozniak@intel.com>
2022-10-26 09:45:00 +0200

    Increase timeout for creating connection to debugger socket

--- gprofiler/profilers/node.py
+++ gprofiler/profilers/node.py
@@ -159,7 +159,7 @@ def _validate_pid(expected_pid: int, sock: WebSocket) -> None:
 
 def create_debugger_socket(nspid: int, ns_link_name: str) -> WebSocket:
     debugger_url = _get_debugger_url()
-    sock = create_connection(debugger_url)
+    sock = create_connection(url=debugger_url, timeout=15.0)
     sock.settimeout(10)
     _validate_ns_node(sock, ns_link_name)
     _validate_pid(nspid, sock)
