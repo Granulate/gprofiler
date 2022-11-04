2de3f9b9192bf7c513d9ea2b6195953f86dbcff5
Marcin Olszewski <marcin.olszewski@intel.com>
2022-11-04 14:08:47 +0100

    Implement reporting DSO name from pyperf

--- gprofiler/profilers/python_ebpf.py
+++ gprofiler/profilers/python_ebpf.py
@@ -73,6 +73,7 @@ class PythonEbpfProfiler(ProfilerBase):
         self.user_stacks_pages = user_stacks_pages
         self._kernel_offsets: Dict[str, int] = {}
         self._metadata = python.PythonMetadata(self._stop_event)
+        self._insert_dso_name = insert_dso_name
 
     @classmethod
     def _pyperf_error(cls, process: Popen) -> NoReturn:
@@ -178,6 +179,8 @@ class PythonEbpfProfiler(ProfilerBase):
             str(self._SYMBOLS_MAP_SIZE),
             # Duration is irrelevant here, we want to run continuously.
         ] + self._offset_args()
+        if self._insert_dso_name:
+            cmd.extend(["--do-insert-dso-name"])
 
         if self.user_stacks_pages is not None:
             cmd.extend(["--user-stacks-pages", str(self.user_stacks_pages)])
--- scripts/pyperf_build.sh
+++ scripts/pyperf_build.sh
@@ -5,7 +5,7 @@
 #
 set -euo pipefail
 
-git clone --depth 1 -b v1.2.3 https://github.com/Granulate/bcc.git && cd bcc && git reset --hard 257430e792575b131441c776ea44e9e80bad6396
+git clone --depth 1 -b report-DSO-from-pyperf https://github.com/marcin-ol/bcc.git && cd bcc && git reset --hard dec6eb3938bff453b34e32d0032e06248d14425d
 
 # (after clone, because we copy the licenses)
 # TODO support aarch64
