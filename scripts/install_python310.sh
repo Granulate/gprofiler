e1d04f6ae685939e9c37b8384b4ce05785be1170
pfilipko1 <piotr.filipkowski@intel.com>
2022-11-21 15:55:38 +0100

    Quote this...

--- scripts/install_python310.sh
+++ scripts/install_python310.sh
@@ -9,4 +9,4 @@ wget https://www.python.org/ftp/python/3.10.8/Python-3.10.8.tgz
 tar -xzf Python-3.10.8.tgz
 cd Python-3.10.8
 ./configure --enable-optimizations --enable-shared --prefix=/usr LDFLAGS="-Wl,-rpath /usr/lib" --with-openssl=/usr
-make python install -j $(nproc)
+make python install -j "$(nproc)"
