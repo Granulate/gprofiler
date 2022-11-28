afbd501e49e1c6b0e1bffd649efca21f23ba3925
Marcin Poźniak <marcin.pozniak@intel.com>
2022-11-28 17:02:25 +0100

    Touch required files in aarch64

--- scripts/node_builder_glibc_env.sh
+++ scripts/node_builder_glibc_env.sh
@@ -4,16 +4,20 @@
 # Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
 #
 set -eu
+# TODO support aarch64
+if [ "$(uname -m)" != "aarch64" ]; then
 
-yum update -y && yum install -y curl python3 make git ca-certificates npm
-# yum has node v10 by default, so we need to add newer version to run node-gyp
-curl -fsSL https://rpm.nodesource.com/setup_16.x | bash -
-yum install -y nodejs
-# node-gyp needs python 3.7+, so we need to install it
-yum -y install openssl-devel bzip2-devel
-curl -L https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz -o Python-3.7.9.tgz
-tar xzf Python-3.7.9.tgz
-cd Python-3.7.9
-./configure --enable-optimizations
-make altinstall
-cd ..
+    yum update -y && yum install -y curl python3 make git ca-certificates npm
+    # yum has node v10 by default, so we need to add newer version to run node-gyp
+    curl -fsSL https://rpm.nodesource.com/setup_16.x | bash -
+    yum install -y nodejs
+    # node-gyp needs python 3.7+, so we need to install it
+    yum -y install openssl-devel bzip2-devel
+    curl -L https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz -o Python-3.7.9.tgz
+    tar xzf Python-3.7.9.tgz
+    cd Python-3.7.9
+    ./configure --enable-optimizations
+    make altinstall
+    cd ..
+
+fi
