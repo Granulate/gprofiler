200c01089b8e27b0cc5710de130413f0896aa6cf
Marcin Olszewski <marcin.olszewski@intel.com>
2022-12-07 09:19:21 +0100

    Include PyPerf v1.2.6

--- scripts/pyperf_build.sh
+++ scripts/pyperf_build.sh
@@ -5,7 +5,7 @@
 #
 set -euo pipefail
 
-git clone --depth 1 -b improve-dso-name-lookup https://github.com/marcin-ol/bcc.git && cd bcc && git reset --hard 7b5c71431903e9031275e1cde49ae6b799df7bde
+git clone --depth 1 -b v1.2.6 https://github.com/Granulate/bcc.git && cd bcc && git reset --hard 6a995642d12287f26cbb97edd3d29e62a5fde337
 
 # (after clone, because we copy the licenses)
 # TODO support aarch64
