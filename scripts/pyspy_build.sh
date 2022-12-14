f2c39ced3c71037d5cf8b10738d4c9614cb7373e
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2022-12-15 01:14:02 +0200

    fix

--- scripts/pyspy_build.sh
+++ scripts/pyspy_build.sh
@@ -3,7 +3,7 @@
 # Copyright (c) Granulate. All rights reserved.
 # Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
 #
-set -exuo pipefail
+set -exu
 
 git clone --depth 1 -b "$(cat pyspy_tag.txt)" https://github.com/Granulate/py-spy.git && git -C py-spy reset --hard "$(cat pyspy_commit.txt)"
 cd py-spy
