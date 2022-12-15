8a6a10267d94ed0cd50a2b15a64f859254edb957
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2022-12-15 12:30:55 +0200

    update

--- scripts/rbspy_build.sh
+++ scripts/rbspy_build.sh
@@ -5,7 +5,7 @@
 #
 set -eu
 
-git clone --depth 1 -b rbspy-upgrade https://github.com/Granulate/rbspy.git
+git clone --depth 1 -b v0.12.1g1 https://github.com/Granulate/rbspy.git
 git -C rbspy reset --hard dd4518391de1cf41e0615174541c17542efc96b9
 cd rbspy
 cargo build --release --target="$(uname -m)"-unknown-linux-musl
