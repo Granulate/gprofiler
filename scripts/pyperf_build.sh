c0d3d995d43a84d51d1832fe60d7937204eaf248
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2022-12-17 02:21:28 +0200

    update build

--- scripts/pyperf_build.sh
+++ scripts/pyperf_build.sh
@@ -5,7 +5,7 @@
 #
 set -euo pipefail
 
-git clone --depth 1 -b aarch64 https://github.com/Granulate/bcc.git && cd bcc && git reset --hard 747cd10fc32514a221d89547e4ca8b1d2b10843d
+git clone --depth 1 -b aarch64 https://github.com/Granulate/bcc.git && cd bcc && git reset --hard 704c7f4334b3562bc4bb8e93e139ad0b211005fe
 
 mkdir build
 cd build
