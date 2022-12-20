3d8e0d4ff96f2fdf0e65b72e28be35960d811662
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2022-12-20 17:32:22 +0200

    java: async-profiler: BCI

--- scripts/async_profiler_build_shared.sh
+++ scripts/async_profiler_build_shared.sh
@@ -5,8 +5,8 @@
 #
 set -euo pipefail
 
-VERSION=v2.9g3
-GIT_REV="26d2a66f08e309f7aa399afb62d1270af5514cd3"
+VERSION=bci-test
+GIT_REV="4d587e2"
 
 git clone --depth 1 -b "$VERSION" https://github.com/Granulate/async-profiler.git && cd async-profiler && git reset --hard "$GIT_REV"
 make all
