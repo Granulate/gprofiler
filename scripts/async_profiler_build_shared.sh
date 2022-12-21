6f6ce990a66f1acfdf562805f86ab064221395d8
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2022-12-21 11:20:13 +0200

    java: Upgrade async-profiler to v2.9g4
    Includes the fix https://github.com/jvm-profiling-tools/async-profiler/commit/ebda293a42c7db0b5918b0ef07ac6546958c764a.

--- scripts/async_profiler_build_shared.sh
+++ scripts/async_profiler_build_shared.sh
@@ -5,8 +5,8 @@
 #
 set -euo pipefail
 
-VERSION=v2.9g3
-GIT_REV="26d2a66f08e309f7aa399afb62d1270af5514cd3"
+VERSION=v2.9g4
+GIT_REV="ebda293a42c7db0b5918b0ef07ac6546958c764a"
 
 git clone --depth 1 -b "$VERSION" https://github.com/Granulate/async-profiler.git && cd async-profiler && git reset --hard "$GIT_REV"
 make all
