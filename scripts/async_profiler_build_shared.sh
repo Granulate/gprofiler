be30ee968b6e0a731cf4cb72a54327868f1f2783
Marcin Olszewski <marcin.olszewski@intel.com>
2023-01-24 13:41:49 +0100

    Fix typo

--- scripts/async_profiler_build_shared.sh
+++ scripts/async_profiler_build_shared.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 VERSION=v2.9g5
 GIT_REV="60cbc128a752c657e8a97300784686e08c8dbe9d"
 
-git clone --depth 1 -b "$VERSION" https://github.com/granulate/async-profiler.git && cd async-profiler && git reset --hard "$GIT_REV"
+git clone --depth 1 -b "$VERSION" https://github.com/Granulate/async-profiler.git && cd async-profiler && git reset --hard "$GIT_REV"
 make all
 
 # add a version file to the build directory
