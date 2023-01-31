acb71d7d5583ebd70d8fdff32504683277845352
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2023-01-31 15:51:56 +0200

    perf: Cleanups & robustness improvements (#666)
    * Clean up logging (pass stdout & stderr as extras, use standard "perf ({type} mode)" in all logs)
    * Clean up leftover files before starting - leftover files are moved by perf to "xxx.old" which is
      later identified by our wait_for_file_by_prefix(), which we don't want.
    * Refactor check_if_needs_restart() to check_if_needs_memory_restart(), to signify its purpose.
    * Check if perf is running every cycle beginning, and re-start it if not. perf might be killed e.g
      due to OOM, and we had nothing in place to re-start it. This means all future gProfiler cycles would
      fails in this run :/

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -141,7 +141,7 @@ def test_nodejs(
         perf_inject=True,
         perf_dwarf_stack_size=0,
         perf_node_attach=False,
-        perf_restart=False,
+        perf_memory_restart=False,
     ) as profiler:
         process_collapsed = snapshot_pid_collapsed(profiler, application_pid)
         assert_collapsed(process_collapsed)
