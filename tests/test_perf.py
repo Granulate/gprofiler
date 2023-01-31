11b84c763dc9058df67491e1ba9fbbc6c2778144
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2023-01-31 02:46:22 +0200

    tests: Add test_perf_restarted_if_killed()

--- tests/test_perf.py
+++ tests/test_perf.py
@@ -3,15 +3,18 @@
 # Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
 #
 
+import logging
 from pathlib import Path
 from threading import Event
 from typing import cast
 
 import pytest
 from docker.models.containers import Container
+from pytest import LogCaptureFixture
 
 from gprofiler.consts import CPU_PROFILING_MODE
 from gprofiler.profilers.perf import DEFAULT_PERF_DWARF_STACK_SIZE, SystemProfiler
+from gprofiler.utils import wait_event
 from tests.utils import (
     assert_function_in_collapsed,
     is_function_in_collapsed,
@@ -185,3 +188,57 @@ def test_dso_name_in_perf_profile(
         collapsed = snapshot_pid_profile(profiler, application_pid).stacks
         assert is_function_in_collapsed("recursive", collapsed)
         assert insert_dso_name == is_function_in_collapsed("recursive (/native)", collapsed)
+
+
+@pytest.mark.parametrize("in_container", [True])
+@pytest.mark.parametrize("perf_mode", ["smart"])
+def test_perf_restarted_if_killed(
+    system_profiler: SystemProfiler,
+    caplog: LogCaptureFixture,
+    in_container: bool,
+) -> None:
+    caplog.set_level(logging.DEBUG)
+    with system_profiler as profiler:
+        # both perfs started
+        assert len(profiler._perfs) == 2
+        assert len(list(filter(lambda r: r.message == "Starting perf (fp mode)", caplog.records))) == 1
+        assert len(list(filter(lambda r: r.message == "Starting perf (dwarf mode)", caplog.records))) == 1
+
+        # kill both
+        for perf in profiler._perfs:
+            assert perf._process is not None
+            perf._process.terminate()
+
+        # wait for them to exit
+        wait_event(20, Event(), lambda: all(not perf.is_running() for perf in profiler._perfs))
+
+        # snapshot - perfs are restarted after a cycle
+        profiler.snapshot()
+
+        # they should restart
+        assert all(perf.is_running() for perf in profiler._perfs)
+        assert (
+            len(
+                list(
+                    filter(
+                        lambda r: r.message == "perf (fp mode) not running (unexpectedly), restarting...",
+                        caplog.records,
+                    )
+                )
+            )
+            == 1
+        )
+        assert (
+            len(
+                list(
+                    filter(
+                        lambda r: r.message == "perf (dwarf mode) not running (unexpectedly), restarting...",
+                        caplog.records,
+                    )
+                )
+            )
+            == 1
+        )
+        # starting message again (now appears twice)
+        assert len(list(filter(lambda r: r.message == "Starting perf (fp mode)", caplog.records))) == 2
+        assert len(list(filter(lambda r: r.message == "Starting perf (dwarf mode)", caplog.records))) == 2
