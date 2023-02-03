76c35968d6e85afb4f91c91424422242c9afd3ef
Marcin Olszewski <marcin.olszewski@intel.com>
2023-02-03 18:28:06 +0100

    Read and record async-profiler version for testing profiler's output against it

--- tests/test_java.py
+++ tests/test_java.py
@@ -25,6 +25,7 @@ from granulate_utils.java import parse_jvm_version
 from granulate_utils.linux.ns import get_process_nspid
 from granulate_utils.linux.process import is_musl
 from packaging.version import Version
+from psutil import Process
 from pytest import LogCaptureFixture, MonkeyPatch
 
 from gprofiler.profilers.java import (
@@ -80,6 +81,19 @@ class AsyncProfiledProcessForTests(AsyncProfiledProcess):
         self._run_async_profiler(
             self._get_base_cmd() + [f"status,log={self._log_path_process},file={self._output_path_process}"],
         )
+
+    def read_ap_version(self: AsyncProfiledProcess) -> str:
+        # call async-profiler 'version' action to discover its version number
+        # read output from AsyncProfiledProcess, which should be empty
+        output_without_version = cast_away_optional(self.read_output())
+        assert output_without_version == ""
+        self._run_async_profiler(
+            self._get_base_cmd() + [f"version" f"{self._get_ap_output_args()}" f",log={self._log_path_process}"]
+        )
+        # async-profiler emits version number compiled into itself
+        output_with_version = cast_away_optional(self.read_output())
+        version = output_with_version.strip()
+        return version
 
 
 def test_async_profiler_already_running(
@@ -753,13 +767,24 @@ def test_no_stray_output_in_stdout_stderr(
         duration=3,
         frequency=999,
     ) as profiler:
+        # read ap-version from test ap-process to match the proper output against it
+        with AsyncProfiledProcessForTests(
+            process=Process(application_pid),
+            storage_dir=profiler._storage_dir,
+            insert_dso_name=False,
+            stop_event=profiler._stop_event,
+            mode="itimer",
+            ap_safemode=0,
+            ap_args="",
+        ) as ap_proc:
+            ap_version = ap_proc.read_ap_version()
         collapsed = snapshot_pid_collapsed(profiler, application_pid)
         assert_collapsed(collapsed)
     application_docker_container.stop(timeout=3)
     application_docker_container.wait(timeout=3)
     textout = application_docker_container.logs(stdout=True, stderr=False).decode()
     texterr = application_docker_container.logs(stdout=False, stderr=True).decode()
-    # output from Fibonacci should be the only output in stdout
-    assert textout.strip() == "Fibonacci thread starting"
+    # output from Fibonacci and async-profiler version should be the only lines  in stdout
+    assert textout.splitlines() == ["Fibonacci thread starting", ap_version]
     # output from Fibonacci should be the only expected output in stderr
     assert texterr.strip() == "Fibonacci loop starting"
