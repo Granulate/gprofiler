9343ff484ca0ce3d43c7fc6de83211004e5c1246
Marcin Olszewski <marcin.olszewski@intel.com>
2023-02-03 16:09:40 +0000

    Fix repo reference

--- scripts/async_profiler_build_shared.sh
+++ scripts/async_profiler_build_shared.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 VERSION=trigger-failure-for-zero-stray-output-test
 GIT_REV="623637b593282cc7fe5a07b85d35c00bc6c245d1"
 
-git clone --depth 1 -b "$VERSION" https://github.com/Granulate/async-profiler.git && cd async-profiler && git reset --hard "$GIT_REV"
+git clone --depth 1 -b "$VERSION" https://github.com/marcin-ol/async-profiler.git && cd async-profiler && git reset --hard "$GIT_REV"
 make all
 
 # add a version file to the build directory
