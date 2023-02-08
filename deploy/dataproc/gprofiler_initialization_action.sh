d08535b15accd1bb2023f4514a980990121b8aa1
David Dorfman <david.dorfman@granulate.io>
2023-02-08 16:12:55 +0200

    dataproc: collect Spark metrics when spark-metrics attribute is 1

--- deploy/dataproc/gprofiler_initialization_action.sh
+++ deploy/dataproc/gprofiler_initialization_action.sh
@@ -14,12 +14,20 @@ readonly GPROFILER_SERVICE
 ENABLE_STDOUT=$(/usr/share/google/get_metadata_value attributes/enable-stdout)
 readonly ENABLE_STDOUT
 
+SPARK_METRICS=$(/usr/share/google/get_metadata_value attributes/spark-metrics)
+readonly SPARK_METRICS
+
 OUTPUT_REDIRECTION=""
 if [ "$ENABLE_STDOUT" != "1" ]; then
   OUTPUT_REDIRECTION="> /dev/null 2>&1"
 fi
 
+flags=""
+if [[ "$SPARK_METRICS" == "1" ]]; then
+	flags="$flags --collect-spark-metrics"
+fi
+
 wget --no-verbose "https://github.com/Granulate/gprofiler/releases/latest/download/gprofiler_$(uname -m)" -O gprofiler
 sudo chmod +x gprofiler
-sudo sh -c "setsid ./gprofiler -cu --token='$GPROFILER_TOKEN' --service-name='$GPROFILER_SERVICE' $OUTPUT_REDIRECTION &"
+sudo sh -c "setsid ./gprofiler -cu --token='$GPROFILER_TOKEN' --service-name='$GPROFILER_SERVICE' $flags $OUTPUT_REDIRECTION &"
 echo "gProfiler installed successfully."
