7ff13c85a6da3949b42f9475497438b511a27487
David Dorfman <david.dorfman@granulate.io>
2023-02-13 12:52:46 +0200

    double quote url argument

--- deploy/emr/gprofiler_action.sh
+++ deploy/emr/gprofiler_action.sh
@@ -6,7 +6,7 @@ test $EUID = 0 || exec sudo "$0" "$@"
 version=latest
 arch=$(uname -m)
 
-wget https://github.com/Granulate/gprofiler/releases/$version/download/gprofiler_$arch -O gprofiler
+wget "https://github.com/Granulate/gprofiler/releases/$version/download/gprofiler_$arch" -O gprofiler
 chmod +x gprofiler
 # Must supply --token=... and --service-name=... arguments when creating cluster
 setsid ./gprofiler -cu "$@" >/dev/null 2>&1 &
