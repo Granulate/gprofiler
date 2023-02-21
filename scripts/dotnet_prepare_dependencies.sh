370bb0b7fe5e9ccdc80e6b368b654e22945b9145
pfilipko1 <piotr.filipkowski@intel.com>
2023-02-21 21:04:48 +0100

    read option

--- scripts/dotnet_prepare_dependencies.sh
+++ scripts/dotnet_prepare_dependencies.sh
@@ -6,6 +6,6 @@
 set -euo pipefail
 
 mkdir -p /tmp/dotnet/deps
-while read i  ; do
+while read -r i  ; do
    cp "/usr/share/dotnet/shared/Microsoft.NETCore.App/6.0.7/$i" "/tmp/dotnet/deps/$i"
 done <./dotnet_trace_dependencies.txt
