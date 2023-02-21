74cbffd1cc4c7e9ddac4999743619aebe6cd3927
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2023-02-21 13:41:00 +0200

    tests: dotnet: xfail test_executable_not_privileged (#698)
    See https://github.com/Granulate/gprofiler/issues/697

--- tests/test_app_metadata.py
+++ tests/test_app_metadata.py
@@ -82,14 +82,17 @@ from tests.utils import run_gprofiler_in_container_for_one_session
             },
         ),
         (
-            True,
-            "dotnet",
-            "dotnet-trace",
-            {
-                "dotnet_version": "6.0.302",
-                "exe": "/usr/share/dotnet/dotnet",
-                "execfn": "/usr/bin/dotnet",
-            },
+            pytest.param(
+                True,
+                "dotnet",
+                "dotnet-trace",
+                {
+                    "dotnet_version": "6.0.302",
+                    "exe": "/usr/share/dotnet/dotnet",
+                    "execfn": "/usr/bin/dotnet",
+                },
+                marks=pytest.mark.xfail(reason="https://github.com/Granulate/gprofiler/issues/697"),
+            )
         ),
     ],
 )
