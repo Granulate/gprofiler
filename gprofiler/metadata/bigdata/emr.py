f5b4558fa0fe301f2a54bfb695f38e359b1c3dfa
slicklash <slicklash@gmail.com>
2023-02-24 11:18:47 +0200

    bigdata: detect provider and version

---
+++ gprofiler/metadata/bigdata/emr.py
@@ -0,0 +1,19 @@
+import json
+from typing import Dict, Optional
+
+
+def _get_instance_data() -> Optional[Dict[str, str]]:
+    try:
+        with open("/mnt/var/lib/info/extraInstanceData.json", "r") as f:
+            obj = json.loads(f.read())
+            if isinstance(obj, dict):
+                return obj
+    except FileNotFoundError:
+        pass
+    return None
+
+
+def get_emr_version() -> Optional[str]:
+    if (data := _get_instance_data()) is not None:
+        return data["releaseLabel"]
+    return None
