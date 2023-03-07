05e7970b92e3deace14c9d99eca1d656fc5f3c06
pfilipko1 <piotr.filipkowski@intel.com>
2023-03-07 12:06:03 +0100

    py-spy 2.7 uwsgi

--- tests/test_python.py
+++ tests/test_python.py
@@ -88,6 +88,9 @@ def test_python_matrix(
     if python_version == "2.7" and profiler_type == "pyperf" and app == "uwsgi":
         pytest.xfail("This combination fails, see https://github.com/Granulate/gprofiler/issues/485")
 
+    if python_version == "2.7" and profiler_type == "py-spy" and app == "uwsgi":
+        pytest.xfail("This combination fails, see <placeholder>")
+
     if python_version in ["2.7", "3.7", "3.8", "3.9", "3.10"] and profiler_type == "py-spy" and libc == "musl":
         pytest.xfail("This combination fails, see <placeholder>")
 
