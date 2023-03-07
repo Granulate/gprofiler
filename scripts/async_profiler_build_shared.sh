8564f6eceb55fd045c95556d9540f53f31ac9c58
Marcin Poźniak <marcin.pozniak@intel.com>
2023-03-07 15:02:03 +0100

    Change url of async-profiler

--- scripts/async_profiler_build_shared.sh
+++ scripts/async_profiler_build_shared.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 VERSION=add_method_modifiers
 GIT_REV="664d04f18da2205b4f34ce422ec144dd2974906e"
 
-git clone --depth 1 -b "$VERSION" https://github.com/Granulate/async-profiler.git && cd async-profiler && git reset --hard "$GIT_REV"
+git clone --depth 1 -b "$VERSION" https://github.com/mpozniak95/async-profiler.git && cd async-profiler && git reset --hard "$GIT_REV"
 
 make all CXXFLAGS="-O3 -fno-omit-frame-pointer -fvisibility=hidden -std=c++11"
 
