c1274e41eb8d8804aba40e3e63bb861e01362a5a
Yonatan Goldschmidt <yon.goldschmidt@gmail.com>
2023-03-15 13:46:42 +0200

    Update scripts/async_profiler_build_shared.sh

--- scripts/async_profiler_build_shared.sh
+++ scripts/async_profiler_build_shared.sh
@@ -9,7 +9,6 @@ VERSION=v2.9g6
 GIT_REV="4938ce815be597fafc6a7a185a16ff394b9d7f41"
 
 git clone --depth 1 -b "$VERSION" https://github.com/Granulate/async-profiler.git && cd async-profiler && git reset --hard "$GIT_REV"
-
 make all
 
 # add a version file to the build directory
