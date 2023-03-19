916f0b5542aabad45d95bdfb2c981542b52dd6c8
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2023-03-19 19:49:41 +0200

    python: pyperf: Allow controlling PyPerf verbosity (#708)
    Closes: https://github.com/Granulate/gprofiler/issues/516

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -151,7 +151,7 @@ def test_python_ebpf(
     profiler_state: ProfilerState,
 ) -> None:
     _ = assert_app_id  # Required for mypy unused argument warning
-    with PythonEbpfProfiler(1000, 5, profiler_state, add_versions=True) as profiler:
+    with PythonEbpfProfiler(1000, 5, profiler_state, add_versions=True, verbose=False) as profiler:
         try:
             process_collapsed = snapshot_pid_collapsed(profiler, application_pid)
         except UnicodeDecodeError as e:
