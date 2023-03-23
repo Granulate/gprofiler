69d98b917220015b65cf481bde8536a81e4be92b
Roi <roi.agai@granulate.io>
2023-03-23 21:37:22 +0200

    Support SA mode and low cardinality (#718)
    * metrics: Fix timestamp
    
    * init commit, just copied stuff from orig PR and tried, exceptions occured so removing concurrency
    
    * removed concurrency, testing
    
    * testing again
    
    * testing
    
    * Fixes, works
    
    * removing old stuff from concurrency
    
    * remove irrelevant
    
    * Adding RDD metrics if low cardinality disabled
    
    * irrelevant whitespaces
    
    * lint fix
    
    * Renaming SPARK_AGGREGATED_METRICS to SPARK_AGGREGATED_STAGE_METRICS
    
    * remove unnecessery const
    
    * remove unnecessery prints
    
    * Remove running application metric out of stage metrics function, also comment removed
    
    * remove old driver master application guess, irrelevant
    
    * fix doc
    
    * resolves https://github.com/Granulate/gprofiler/pull/718\#discussion_r1133932971
    
    * resolves https://github.com/Granulate/gprofiler/pull/718\#discussion_r1133973155
    
    * resolves https://github.com/Granulate/gprofiler/pull/718\#discussion_r1133949815
    
    * works
    
    * lint
    
    * some irrelevant task summaries, small lint
    
    * executor count includes driver
    
    * resolves https://github.com/Granulate/gprofiler/pull/718\#discussion_r1141401829
    
    * resolves https://github.com/Granulate/gprofiler/pull/718\#discussion_r1141403321
    
    * resolves https://github.com/Granulate/gprofiler/pull/718\#discussion_r1141405392
    
    * resolves https://github.com/Granulate/gprofiler/pull/718\#discussion_r1141403722
    
    * resolves https://github.com/Granulate/gprofiler/pull/718\#discussion_r1141406974
    
    * lint
    
    * Add master address json object code reference doc
    
    * SPARK_MASTER_STATE_PATH remove trailing slash
    
    * explicit doc regarding executors count metric
    
    * empty list by default when accessing
    
    * remove redundant log message
    
    * remove redundant str casting to format fields
    
    * get master process argument value implementation
    
    * handle 404 http status codes
    
    * lint
    
    * no reason for KeyError handle
    
    * remove redundant exception logging
    
    * remove more redundant logger.exception args
    
    * lint
    
    ---------
    
    Co-authored-by: David Dorfman <david.dorfman@granulate.io>

--- gprofiler/spark/metrics.py
+++ gprofiler/spark/metrics.py
@@ -85,9 +85,6 @@ SPARK_STAGE_METRICS = {
         "diskBytesSpilled",
     )
 }
-SPARK_RDD_METRICS = {
-    metric: f"spark_rdd_{metric}" for metric in ("numPartitions", "numCachedPartitions", "memoryUsed", "diskUsed")
-}
 SPARK_TASK_SUMMARY_METRICS = {
     metric: f"spark_stage_tasks_summary_{metric}"
     for metric in (
@@ -158,3 +155,11 @@ SPARK_STREAMING_BATCHES_METRICS = {
     "avg25_batchDuration": "spark_app_streaming_batchDuration_avg25",
 }
 SPARK_EXECUTORS_METRICS = {"count": "spark_executors_count", "activeCount": "spark_executors_active_count"}
+SPARK_AGGREGATED_STAGE_METRICS = {
+    "failed_tasks": "spark_aggregated_stage_failed_tasks",
+    "active_tasks": "spark_aggregated_stage_active_tasks",
+    "pending_stages": "spark_aggregated_stage_pending_stages",
+    "failed_stages": "spark_aggregated_stage_failed_stages",
+    "active_stages": "spark_aggregated_stage_active_stages",
+}
+SPARK_RUNNING_APPS_COUNT_METRIC = "spark_num_applications_running"
