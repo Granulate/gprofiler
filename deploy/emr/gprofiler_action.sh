bc88aeae02d78886c1eb17f901632c0d61cf8ca6
Yonatan Goldschmidt <yonatan.goldschmidt@granulate.io>
2023-03-23 19:30:10 +0200

    Add "exit 0" to all Big Data installation commands

--- deploy/emr/gprofiler_action.sh
+++ deploy/emr/gprofiler_action.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Re-run as root:
-test $EUID = 0 || exec sudo "$0" "$@"
+(
+    # Re-run as root:
+    test $EUID = 0 || exec sudo "$0" "$@"
 
-version=latest
-arch=$(uname -m)
+    version=latest
+    arch=$(uname -m)
 
-wget "https://github.com/Granulate/gprofiler/releases/$version/download/gprofiler_$arch" -O gprofiler
-chmod +x gprofiler
-# Must supply --token=... and --service-name=... arguments when creating cluster
-setsid ./gprofiler -cu "$@" >/dev/null 2>&1 &
+    wget "https://github.com/Granulate/gprofiler/releases/$version/download/gprofiler_$arch" -O gprofiler
+    chmod +x gprofiler
+    # Must supply --token=... and --service-name=... arguments when creating cluster
+    setsid ./gprofiler -cu "$@" >/dev/null 2>&1 &
+)
+exit 0  # ensures we never block the EMR node from starting.
