3707655e48cdc1093cfe6646d388b45cbb7930e3
pfilipko1 <piotr.filipkowski@intel.com>
2023-04-11 10:40:41 +0200

    output?

--- tests/test_executable.py
+++ tests/test_executable.py
@@ -129,6 +129,7 @@ def test_executable_not_privileged(
     exit_code, output = application_docker_container.exec_run(cmd=command, privileged=False, user="root:root")
 
     _no_errors(output.decode())
+    print(output.decode())
     assert exit_code == 0
 
     collapsed = parse_one_collapsed(Path(output_directory / "last_profile.col").read_text())
