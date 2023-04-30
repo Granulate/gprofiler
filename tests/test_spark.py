b29f95b14826b02181c4a34e93e3e2aa8b5e8d90
Roi Agai <roi.agai@granulate.io>
2023-04-30 16:15:54 +0300

    fixture default function

--- tests/test_spark.py
+++ tests/test_spark.py
@@ -46,7 +46,7 @@ DISCOVER_INTERVAL_SECS = 5
 DISCOVER_TIMEOUT_SECS = 60
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 def sparkpi_container(docker_client: DockerClient, application_docker_mounts: List[Mount]) -> Container:
     """
     This fixture is responsible for running SparkPi application in a container.
