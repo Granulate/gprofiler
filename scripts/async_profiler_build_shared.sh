43d7dee894e2709882aa19ec36a903f2e2333050
Marcin Olszewski <marcin.olszewski@intel.com>
2023-05-10 13:35:28 +0200

    Update to async-profiler v2.10g1

--- scripts/async_profiler_build_shared.sh
+++ scripts/async_profiler_build_shared.sh
@@ -5,10 +5,10 @@
 #
 set -euo pipefail
 
-VERSION=tryout-asprof-jattach-passthru
-GIT_REV="355000e0cbf8888730628c7247c6eccf940f910b"
+VERSION=v2.10g1
+GIT_REV="b3baf6010e31378679a28fe1cd30168f51cc1ade"
 
-git clone --depth 1 -b "$VERSION" https://github.com/marcin-ol/async-profiler.git && cd async-profiler && git reset --hard "$GIT_REV"
+git clone --depth 1 -b "$VERSION" https://github.com/Granulate/async-profiler.git && cd async-profiler && git reset --hard "$GIT_REV"
 make all
 
 # add a version file to the build directory
