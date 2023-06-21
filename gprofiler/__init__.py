d2141c1cbdfcad5be66ac86a9ca21c60999b270b
David Dorfman <david.dorfman@granulate.io>
2023-06-21 11:14:16 +0300

    gProfiler 1.30.0 & reenable staticx for Aarch64 (#806)
    PR #622 accidentally removed invocation of staticx on Aarch64 builds.

--- gprofiler/__init__.py
+++ gprofiler/__init__.py
@@ -2,4 +2,4 @@
 # Copyright (c) Granulate. All rights reserved.
 # Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
 #
-__version__ = "1.29.0"
+__version__ = "1.30.0"
