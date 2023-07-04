461bc190fe48bbaaef242c0c28fd9a8e2c5bc962
Marcin Poźniak <marcin.pozniak@intel.com>
2023-07-04 12:49:07 +0200

    Remove -b for testing PR in CI/CD

--- scripts/async_profiler_build_shared.sh
+++ scripts/async_profiler_build_shared.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 VERSION=v2.10g1
 GIT_REV="6c161dc29c46e8dcc89eb5b0dffd5f460febd139"
 
-git clone --depth 1 -b "$VERSION" https://github.com/mpozniak95/async-profiler.git && cd async-profiler && git reset --hard "$GIT_REV"
+git clone --depth 1 https://github.com/mpozniak95/async-profiler.git && cd async-profiler && git reset --hard "$GIT_REV"
 make all
 
 # add a version file to the build directory
