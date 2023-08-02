60e6617ac5a97f623607392c81f2dc1712733994
Marcin Poźniak <marcin.pozniak@intel.com>
2023-08-02 10:29:24 +0200

    Check for extract-resources right after parsing cmd args

--- gprofiler/main.py
+++ gprofiler/main.py
@@ -651,7 +651,7 @@ def parse_cmd_args() -> configargparse.Namespace:
     extract_resources.add_argument(
         "--resources-dest",
         dest="resources_dest",
-        default="",
+        default=None,
         help="Path to which the resources will be extracted",
     )
 
@@ -821,7 +821,7 @@ def parse_cmd_args() -> configargparse.Namespace:
     if not args.upload_results and not args.output_dir and not args.extract_resources:
         parser.error("Must pass at least one output method (--upload-results / --output-dir)")
 
-    if args.extract_resources and args.resources_dest == "":
+    if args.extract_resources and args.resources_dest is None:
         parser.error("Must provide --resources-dest when extract-resources")
 
     if args.perf_dwarf_stack_size > 65528:
@@ -972,6 +972,12 @@ def pids_to_processes(args: configargparse.Namespace) -> Optional[List[Process]]
 
 def main() -> None:
     args = parse_cmd_args()
+
+    if hasattr(args, "func"):
+        if args.subcommand == "extract-resources":
+            args.func(args.resources_dest)
+            return
+
     processes_to_profile = pids_to_processes(args)
 
     if is_windows() or get_aws_execution_env() == "AWS_ECS_FARGATE":
@@ -1096,11 +1102,8 @@ def main() -> None:
             spark_sampler = None
 
         if hasattr(args, "func"):
-            if args.subcommand == "extract-resources":
-                args.func(args.resources_dest)
-            else:
-                assert args.subcommand == "upload-file"
-                args.func(args, profiler_api_client)
+            assert args.subcommand == "upload-file"
+            args.func(args, profiler_api_client)
             return
 
         enrichment_options = EnrichmentOptions(
