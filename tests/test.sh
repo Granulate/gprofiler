f56031c4ea98ea990920b20043df74b48dd5c4d9
pfilipko1 <piotr.filipkowski@intel.com>
2023-08-11 09:32:57 +0200

    ignore installed

--- tests/test.sh
+++ tests/test.sh
@@ -34,7 +34,7 @@ if [ ! -f "$PERF_RESOURCE" ] && [[ "$*" != *"--executable"* ]]; then
     exit 1
 fi
 python3 -m pip install -q --upgrade setuptools pip
-python3 -m pip install -r ./requirements.txt -r ./exe-requirements.txt -r ./dev-requirements.txt
+python3 -m pip install --ignore-installed -r ./requirements.txt -r ./exe-requirements.txt -r ./dev-requirements.txt
 
 # TODO: python3 -m pip install .
 sudo env "PATH=$PATH" python3 -m pytest -v tests/ "$@"
