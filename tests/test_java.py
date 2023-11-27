Granulate/gprofiler

commit 6360449252793ad59802a3c09fde297646529b7d

Ensure directories used by gProfiler to host async-profiler DSO are owned by root (#832)

gprofiler/profilers/java.py
@@ -13,7 +13,7 @@ from pathlib import Path
 from subprocess import CompletedProcess
 from threading import Event, Lock
 from types import TracebackType
-from typing import Any, Dict, Iterable, List, Optional, Sequence, Set, Type, TypeVar, Union
+from typing import Any, Dict, Iterable, List, Optional, Set, Type, TypeVar, Union
 
 import psutil
 from granulate_utils.java import (
@@ -79,7 +79,7 @@ from gprofiler.utils import (
     touch_path,
     wait_event,
 )
-from gprofiler.utils.fs import is_rw_exec_dir, safe_copy
+from gprofiler.utils.fs import is_owned_by_root, is_rw_exec_dir, mkdir_owned_root, safe_copy
 from gprofiler.utils.perf import can_i_use_perf_events
 from gprofiler.utils.process import process_comm, search_proc_maps
 
@@ -471,9 +471,10 @@ class AsyncProfiledProcess:
         # because storage_dir changes between runs.
         # we embed the async-profiler version in the path, so future gprofiler versions which use another version
         # of AP case use it (will be loaded as a different DSO)
+        self._ap_dir_base = self._find_rw_exec_dir()
+        self._ap_dir_versioned = os.path.join(self._ap_dir_base, f"async-profiler-{get_ap_version()}")
         self._ap_dir_host = os.path.join(
-            self._find_rw_exec_dir(POSSIBLE_AP_DIRS),
-            f"async-profiler-{get_ap_version()}",
+            self._ap_dir_versioned,
             "musl" if self._needs_musl_ap() else "glibc",
         )
 
@@ -498,20 +499,42 @@ class AsyncProfiledProcess:
         self._collect_meminfo = collect_meminfo
         self._include_method_modifiers = ",includemm" if include_method_modifiers else ""
 
-    def _find_rw_exec_dir(self, available_dirs: Sequence[str]) -> str:
+    def _find_rw_exec_dir(self) -> str:
         """
         Find a rw & executable directory (in the context of the process) where we can place libasyncProfiler.so
         and the target process will be able to load it.
+        This function creates the gprofiler_tmp directory as a directory owned by root, if it doesn't exist under the
+        chosen rwx directory.
+        It does not create the parent directory itself, if it doesn't exist (e.g /run).
+        The chosen rwx directory needs to be owned by root.
         """
-        for d in available_dirs:
-            full_dir = resolve_proc_root_links(self._process_root, d)
+        for d in POSSIBLE_AP_DIRS:
+            full_dir = Path(resolve_proc_root_links(self._process_root, d))
+            if not full_dir.parent.exists():
+                continue  # we do not create the parent.
+
+            if not is_owned_by_root(full_dir.parent):
+                continue  # the parent needs to be owned by root
+
+            mkdir_owned_root(full_dir)
+
             if is_rw_exec_dir(full_dir):
-                return full_dir
+                return str(full_dir)
         else:
-            raise NoRwExecDirectoryFoundError(f"Could not find a rw & exec directory out of {available_dirs}!")
+            raise NoRwExecDirectoryFoundError(
+                f"Could not find a rw & exec directory out of {POSSIBLE_AP_DIRS} for {self._process_root}!"
+            )
 
     def __enter__(self: T) -> T:
-        os.makedirs(self._ap_dir_host, 0o755, exist_ok=True)
+        # create the directory structure for executable libap, make sure it's owned by root
+        # for sanity & simplicity, mkdir_owned_root() does not support creating parent directories, as this allows
+        # the caller to absentmindedly ignore the check of the parents ownership.
+        # hence we create the structure here part by part.
+        assert is_owned_by_root(
+            Path(self._ap_dir_base)
+        ), f"expected {self._ap_dir_base} to be owned by root at this point"
+        mkdir_owned_root(self._ap_dir_versioned)
+        mkdir_owned_root(self._ap_dir_host)
         os.makedirs(self._storage_dir_host, 0o755, exist_ok=True)
 
         self._check_disk_requirements()

gprofiler/main.py
@@ -65,7 +65,7 @@ from gprofiler.utils import (
     resource_path,
     run_process,
 )
-from gprofiler.utils.fs import escape_filename
+from gprofiler.utils.fs import escape_filename, mkdir_owned_root
 from gprofiler.utils.proxy import get_https_proxy
 
 if is_linux():
@@ -954,6 +954,7 @@ def main() -> None:
     if is_windows() or get_aws_execution_env() == "AWS_ECS_FARGATE":
         args.perf_mode = "disabled"
         args.pid_ns_check = False
+
     if args.subcommand != "upload-file":
         verify_preconditions(args, processes_to_profile)
 
@@ -1021,8 +1022,7 @@ def main() -> None:
                 )
                 sys.exit(1)
 
-        if not os.path.exists(TEMPORARY_STORAGE_PATH):
-            os.mkdir(TEMPORARY_STORAGE_PATH)
+        mkdir_owned_root(TEMPORARY_STORAGE_PATH)
 
         try:
             client_kwargs = {}

gprofiler/utils/fs.py
@@ -8,9 +8,10 @@ import os
 import shutil
 from pathlib import Path
 from secrets import token_hex
+from typing import Union
 
 from gprofiler.platform import is_windows
-from gprofiler.utils import remove_path, run_process
+from gprofiler.utils import is_root, remove_path, run_process
 
 
 def safe_copy(src: str, dst: str) -> None:
@@ -23,18 +24,19 @@ def safe_copy(src: str, dst: str) -> None:
     os.rename(dst_tmp, dst)
 
 
-def is_rw_exec_dir(path: str) -> bool:
+def is_rw_exec_dir(path: Path) -> bool:
     """
     Is 'path' rw and exec?
     """
+    assert is_owned_by_root(path), f"expected {path} to be owned by root!"
+
     # randomize the name - this function runs concurrently on paths of in same mnt namespace.
-    test_script = Path(path) / f"t-{token_hex(10)}.sh"
+    test_script = path / f"t-{token_hex(10)}.sh"
 
     # try creating & writing
     try:
-        os.makedirs(path, 0o755, exist_ok=True)
         test_script.write_text("#!/bin/sh\nexit 0")
-        test_script.chmod(0o755)
+        test_script.chmod(0o755)  # make sure it's executable. file is already writable only by root due to umask.
     except OSError as e:
         if e.errno == errno.EROFS:
             # ro
@@ -56,3 +58,41 @@ def is_rw_exec_dir(path: str) -> bool:
 
 def escape_filename(filename: str) -> str:
     return filename.replace(":", "-" if is_windows() else ":")
+
+
+def is_owned_by_root(path: Path) -> bool:
+    statbuf = path.stat()
+    return statbuf.st_uid == 0 and statbuf.st_gid == 0
+
+
+def mkdir_owned_root(path: Union[str, Path], mode: int = 0o755) -> None:
+    """
+    Ensures a directory exists and is owned by root.
+
+    If the directory exists and is owned by root, it is left as is.
+    If the directory exists and is not owned by root, it is removed and recreated. If after recreation
+    it is still not owned by root, the function raises.
+    """
+    assert is_root()  # this function behaves as we expect only when run as root
+
+    path = path if isinstance(path, Path) else Path(path)
+    # parent is expected to be root - otherwise, after we create the root-owned directory, it can be removed
+    # as re-created as non-root by a regular user.
+    if not is_owned_by_root(path.parent):
+        raise Exception(f"expected {path.parent} to be owned by root!")
+
+    if path.exists():
+        if is_owned_by_root(path):
+            return
+
+        shutil.rmtree(path)
+
+    try:
+        os.mkdir(path, mode=mode)
+    except FileExistsError:
+        # likely racing with another thread of gprofiler. as long as the directory is root after all, we're good.
+        pass
+
+    if not is_owned_by_root(path):
+        # lost race with someone else?
+        raise Exception(f"Failed to create directory {str(path)} as owned by root")

commit 6e323a2d4286243ef65435935fbd558621fbc558

java: Fix _find_rw_exec_dir() handling of ro directories
Fixes a regression from 6360449252793ad59802a3c09fde297646529b7d, and improves the tests
so we'd catch it next time :)

gprofiler/profilers/java.py
@@ -2,6 +2,7 @@
 # Copyright (c) Granulate. All rights reserved.
 # Licensed under the AGPL3 License. See LICENSE.md in the project root for license information.
 #
+import errno
 import functools
 import json
 import os
@@ -540,7 +541,13 @@ class AsyncProfiledProcess:
             if not is_owned_by_root(full_dir.parent):
                 continue  # the parent needs to be owned by root
 
-            mkdir_owned_root(full_dir)
+            try:
+                mkdir_owned_root(full_dir)
+            except OSError as e:
+                # dir is not r/w, try next one
+                if e.errno == errno.EROFS:
+                    continue
+                raise
 
             if is_rw_exec_dir(full_dir):
                 return str(full_dir)

tests/conftest.py
@@ -649,7 +649,8 @@ def python_version(in_container: bool, application_docker_container: Container) 
 
 
 @fixture
-def noexec_tmp_dir(in_container: bool, tmp_path: Path) -> Iterator[str]:
+def noexec_or_ro_tmp_dir(in_container: bool, tmp_path: Path, noexec_or_ro: str) -> Iterator[str]:
+    assert noexec_or_ro in ("noexec", "ro")
     if in_container:
         # only needed for non-container tests
         yield ""
@@ -658,7 +659,7 @@ def noexec_tmp_dir(in_container: bool, tmp_path: Path) -> Iterator[str]:
     tmpfs_path = tmp_path / "tmpfs"
     tmpfs_path.mkdir(0o755, exist_ok=True)
     try:
-        subprocess.run(["mount", "-t", "tmpfs", "-o", "noexec", "none", str(tmpfs_path)], check=True)
+        subprocess.run(["mount", "-t", "tmpfs", "-o", noexec_or_ro, "none", str(tmpfs_path)], check=True)
         yield str(tmpfs_path)
     finally:
         subprocess.run(["umount", str(tmpfs_path)], check=True)

tests/test_java.py
@@ -22,12 +22,14 @@ from docker import DockerClient
 from docker.models.containers import Container
 from docker.models.images import Image
 from granulate_utils.java import parse_jvm_version
+from granulate_utils.linux.mountinfo import iter_mountinfo
 from granulate_utils.linux.ns import get_process_nspid
 from granulate_utils.linux.process import is_musl
 from packaging.version import Version
 from psutil import Process
 from pytest import LogCaptureFixture, MonkeyPatch
 
+import gprofiler.profilers.java
 from gprofiler.profiler_state import ProfilerState
 from gprofiler.profilers.java import (
     JAVA_SAFEMODE_ALL,
@@ -38,6 +40,7 @@ from gprofiler.profilers.java import (
     frequency_to_ap_interval,
     get_java_version,
 )
+from gprofiler.utils import GPROFILER_DIRECTORY_NAME
 from tests.conftest import AssertInCollapsed
 from tests.type_utils import cast_away_optional
 from tests.utils import (
@@ -51,6 +54,7 @@ from tests.utils import (
     make_java_profiler,
     snapshot_pid_collapsed,
     snapshot_pid_profile,
+    str_removesuffix,
 )
 
 
@@ -451,48 +455,78 @@ def filter_jattach_load_records(records: List[LogRecord]) -> List[LogRecord]:
 
 
 @pytest.mark.parametrize(
-    "extra_application_docker_mounts",
+    "noexec_or_ro,extra_application_docker_mounts",
     [
-        pytest.param([docker.types.Mount(target="/tmp", source="", type="tmpfs", read_only=False)], id="noexec"),
+        pytest.param(
+            "noexec", [docker.types.Mount(target="/tmpfs", source="", type="tmpfs", read_only=False)], id="noexec"
+        ),
+        pytest.param("ro", [docker.types.Mount(target="/tmpfs", source="", type="tmpfs", read_only=True)], id="ro"),
     ],
 )
-def test_java_noexec_dirs(
-    tmp_path_world_accessible: Path,
+def test_java_noexec_or_ro_dirs(
+    tmp_path_world_accessible: Path,  # will be used by AP for logs & outputs
     application_pid: int,
+    extra_application_docker_mounts: List[docker.types.Mount],
     assert_collapsed: AssertInCollapsed,
     caplog: LogCaptureFixture,
-    noexec_tmp_dir: str,
+    noexec_or_ro_tmp_dir: str,
     in_container: bool,
     monkeypatch: MonkeyPatch,
     profiler_state: ProfilerState,
+    noexec_or_ro: str,
 ) -> None:
     """
     Tests that gProfiler is able to select a non-default directory for libasyncProfiler if the default one
-    is noexec, both container and host.
+    is noexec/ro - not rwx - both container and host.
     """
     caplog.set_level(logging.DEBUG)
 
-    if not in_container:
-        import gprofiler.profilers.java
+    # step 1: configure POSSIBLE_AP_DIRS to try first the noexec/ro dir we've set up for this test.
+    # the first dir will fail the rwx test, and gprofiler will try using the 2nd dir.
+    # the default "first" dir is /tmp, but we don't want mount onto /tmp because it's not legit on a live
+    # on a live system (will hide existing files if /tmp is not tmpfs, as we'll create a new mount),
+    # and making it ro in a container creates other problems. A new mount it is.
+    if in_container:
+        assert len(extra_application_docker_mounts) == 1
+        mount = extra_application_docker_mounts[0]
+        test_dir = mount["Target"]
+    else:
+        test_dir = noexec_or_ro_tmp_dir
+    monkeypatch.setattr(
+        gprofiler.profilers.java,
+        "POSSIBLE_AP_DIRS",
+        (
+            os.path.join(test_dir, GPROFILER_DIRECTORY_NAME),
+            gprofiler.profilers.java.POSSIBLE_AP_DIRS[1],
+        ),
+    )
 
-        run_dir = gprofiler.profilers.java.POSSIBLE_AP_DIRS[1]
-        assert run_dir.startswith("/run")
-        # noexec_tmp_dir won't work and gprofiler will try using run_dir
-        # this is done because modifying /tmp on a live system is not legit (we need to create a new tmpfs
-        # mount because /tmp is not necessarily tmpfs; and that'll hide all current files in /tmp).
-        monkeypatch.setattr(gprofiler.profilers.java, "POSSIBLE_AP_DIRS", (noexec_tmp_dir, run_dir))
+    # step 2: verify that the first dir gprofiler will try is truly mounted with our desired options.
+    # for the sake of the test - we expect it to be a mountpint.
+    mounted_directory = str_removesuffix(
+        gprofiler.profilers.java.POSSIBLE_AP_DIRS[0], f"/{GPROFILER_DIRECTORY_NAME}", assert_suffixed=True
+    )
+    assert (
+        noexec_or_ro
+        in next(filter(lambda m: m.mount_point == mounted_directory, iter_mountinfo(application_pid))).mount_options
+    )
 
+    # step 3: ensure none of the possible dirs share a common path - otherwise, it's possible that
+    # they share a mount, too, and both are noexec/ro.
+    assert os.path.commonpath(gprofiler.profilers.java.POSSIBLE_AP_DIRS) == "/"
+
+    # run a profiling session...
     with make_java_profiler(profiler_state) as profiler:
         assert_collapsed(snapshot_pid_collapsed(profiler, application_pid))
 
-    # should use this path instead of /tmp/gprofiler_tmp/...
-
+    # step 4: ensure we truly used the second dir - POSSIBLE_AP_DIRS[1]
     jattach_loads = filter_jattach_load_records(caplog.records)
     # 2 entries - start and stop
     assert len(jattach_loads) == 2
-    # 3rd part of commandline to AP - shall begin with non-default directory
+    # 3rd part of commandline to AP - shall begin with POSSIBLE_AP_DIRS[1]
     assert all(
-        log_record_extra(jl)["command"][3].startswith("/run/gprofiler_tmp/async-profiler-") for jl in jattach_loads
+        log_record_extra(jl)["command"][3].startswith(f"{gprofiler.profilers.java.POSSIBLE_AP_DIRS[1]}/async-profiler-")
+        for jl in jattach_loads
     )
 
 

tests/utils.py
@@ -408,3 +408,10 @@ def log_record_extra(r: LogRecord) -> Dict[Any, Any]:
     Gets the "extra" attached to a LogRecord
     """
     return getattr(r, "extra", {})
+
+
+def str_removesuffix(s: str, suffix: str, assert_suffixed: bool = True) -> str:
+    suffixed = s.endswith(suffix)
+    if assert_suffixed:
+        assert suffixed
+    return s[: -len(suffix)] if suffixed else s