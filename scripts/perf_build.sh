0f10bb4de759e27e5d543a2783c590508b26c69f
Roi Agai <roi.agai@granulate.io>
2024-02-29 22:27:28 +0200

    Perf: fix v6.7 build on aarch64 (#891)

--- scripts/perf_build.sh
+++ scripts/perf_build.sh
@@ -11,39 +11,6 @@ unzip -qq linux.zip
 rm linux.zip
 cd linux-*/
 
-# building those plugins on Aarch64's requires some of libc.a's (and other libs) .o files to
-# be built "PIC", and in the default installation, they are not.
-# we don't use those plugins (and we don't even deploy them) so this patch disables their build.
-# (I couldn't find any way to do it with a build config to "make -C tools/perf" sadly)
-if [ "$(uname -m)" = "aarch64" ]; then
-    patch -p1 <<'EOF'
-diff --git a/tools/lib/traceevent/plugins/Makefile b/tools/lib/traceevent/plugins/Makefile
---- a/tools/lib/traceevent/plugins/Makefile
-+++ b/tools/lib/traceevent/plugins/Makefile
-@@ -127,19 +127,6 @@ build := -f $(srctree)/tools/build/Makefile.build dir=. obj
-
- DYNAMIC_LIST_FILE := $(OUTPUT)libtraceevent-dynamic-list
-
--PLUGINS  = plugin_jbd2.so
--PLUGINS += plugin_hrtimer.so
--PLUGINS += plugin_kmem.so
--PLUGINS += plugin_kvm.so
--PLUGINS += plugin_mac80211.so
--PLUGINS += plugin_sched_switch.so
--PLUGINS += plugin_function.so
--PLUGINS += plugin_futex.so
--PLUGINS += plugin_xen.so
--PLUGINS += plugin_scsi.so
--PLUGINS += plugin_cfg80211.so
--PLUGINS += plugin_tlb.so
--
- PLUGINS    := $(addprefix $(OUTPUT),$(PLUGINS))
- PLUGINS_IN := $(PLUGINS:.so=-in.o)
-
-EOF
-
-fi
-
 NO_LIBTRACEEVENT=1 NO_JEVENTS=1 make -C tools/perf LDFLAGS=-static -j "$(nproc)" perf
 cp tools/perf/perf /
 # need it static as well, even though it's used only during build (relies on libpcap, ...)
